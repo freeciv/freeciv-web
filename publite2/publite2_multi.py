2a4c246aec1d4681d12ba63b62e5de1811c1eed1
Tom von Schwerdtner <tomvons@gmail.com>
2011-03-07 20:19:00 -0500

    python code cleanup

--- publite2/publite2_multi.py
+++ publite2/publite2_multi.py
@@ -13,29 +13,30 @@ logdir = "/tmp/";
 pubscript = "pubscript_multi.serv";
 servers_count = 5;
 
+
 class civserverproc(Thread):
-  def __init__ (self, port):
-    Thread.__init__(self)
-    self.port = port;
+    def __init__ (self, port):
+        Thread.__init__(self)
+        self.port = port;
 
-  def run(self):
-   while 1:
-    try:
-        retcode = call("civserver --port " + str(self.port) + " -q 20 -e " 
-                       + " -m -M " + metaserver  + " --topic \"Multiplayer\" --read " + pubscript +  " --log " + logdir + "civserver-" + str(self.port) + ".log", shell=True)
-        if retcode < 0:
-            print >>sys.stderr, "Civserver was terminated by signal", -retcode
-        else:
-            print >>sys.stderr, "Civserver returned", retcode
-    except OSError, e:
-        print >>sys.stderr, "Execution failed:", e
+    def run(self):
+        while 1:
+            try:
+                retcode = call("civserver --port " + str(self.port) + " -q 20 -e " 
+                        + " -m -M " + metaserver  + " --topic \"Multiplayer\" --read " + pubscript +  " --log " + logdir + "civserver-" + str(self.port) + ".log", shell=True)
+                if retcode < 0:
+                    print >>sys.stderr, "Civserver was terminated by signal", -retcode
+                else:
+                    print >>sys.stderr, "Civserver returned", retcode
+        except OSError, e:
+            print >>sys.stderr, "Execution failed:", e
 
-    time.sleep(10);
+        time.sleep(10);
 
 
 for i in range(servers_count):
-  current = civserverproc(port);
-  port += 1;
-  current.start()
+    current = civserverproc(port);
+    port += 1;
+    current.start()
 
 print("Civservers done!");

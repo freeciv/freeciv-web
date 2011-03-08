05acd37b3db1ef9a4f2099a442d2caaf908a8678
Tom von Schwerdtner <tomvons@gmail.com>
2011-03-07 20:19:22 -0500

    removing semicolons....

--- publite2/publite2_multi.py
+++ publite2/publite2_multi.py
@@ -6,18 +6,18 @@ from threading import Thread
 import sys
 import time
 
-port = 5655;
+port = 5655
 # FIXME: This must be the hostname and port of resin server.
-metaserver = "http://localhost:8080/freecivmetaserve/metaserver.php";
-logdir = "/tmp/";
-pubscript = "pubscript_multi.serv";
-servers_count = 5;
+metaserver = "http://localhost:8080/freecivmetaserve/metaserver.php"
+logdir = "/tmp/"
+pubscript = "pubscript_multi.serv"
+servers_count = 5
 
 
 class civserverproc(Thread):
     def __init__ (self, port):
         Thread.__init__(self)
-        self.port = port;
+        self.port = port
 
     def run(self):
         while 1:
@@ -31,12 +31,12 @@ class civserverproc(Thread):
         except OSError, e:
             print >>sys.stderr, "Execution failed:", e
 
-        time.sleep(10);
+        time.sleep(10)
 
 
 for i in range(servers_count):
-    current = civserverproc(port);
-    port += 1;
+    current = civserverproc(port)
+    port += 1
     current.start()
 
-print("Civservers done!");
+print("Civservers done!")

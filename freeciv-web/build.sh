4792637485833fa67b338c874bd1cf6ff2030e5a
Marko Lindqvist <cazfi74@gmail.com>
2012-05-17 11:05:59 +0300

    Made build.sh location agnostic

--- freeciv-web/build.sh
+++ freeciv-web/build.sh
@@ -1,4 +1,7 @@
+
+ROOTDIR="$(pwd)/../.."
+
 cd src/main/webapp/ &&
 sh compress-js.sh &&
 cd ../../../ &&
-mvn install && cp target/freeciv-web.war ~/freeciv-build/resin-4.0.24/webapps/ROOT.war && cd ~/freeciv-build/resin-4.0.24/webapps/ && rm -rf ROOT && unzip ROOT.war -d ROOT
+mvn install && cp target/freeciv-web.war "${ROOTDIR}/resin-4.0.27/webapps/ROOT.war" && cd "${ROOTDIR}/resin-4.0.27/webapps/" && rm -rf ROOT && unzip ROOT.war -d ROOT

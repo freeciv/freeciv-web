34a447fb3bd8b6bc83f2c73d9bfd7c8fcc41cbe3
Marko Lindqvist <cazfi74@gmail.com>
2012-07-16 02:11:07 +0300

    Use resin 4.0.29

--- freeciv-web/build.sh
+++ freeciv-web/build.sh
@@ -4,4 +4,4 @@ ROOTDIR="$(pwd)/../.."
 cd src/main/webapp/ &&
 sh compress-js.sh &&
 cd ../../../ &&
-mvn install && cp target/freeciv-web.war "${ROOTDIR}/resin-4.0.27/webapps/ROOT.war" && cd "${ROOTDIR}/resin-4.0.27/webapps/" && rm -rf ROOT && unzip ROOT.war -d ROOT
+mvn install && cp target/freeciv-web.war "${ROOTDIR}/resin-4.0.29/webapps/ROOT.war" && cd "${ROOTDIR}/resin-4.0.29/webapps/" && rm -rf ROOT && unzip ROOT.war -d ROOT

740e6d4394a77e6decb2ef90ecd482610e044805
Marko Lindqvist <cazfi74@gmail.com>
2012-07-16 03:57:38 +0300

    Removed old prepatched freeciv tree from git.

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -3,7 +3,7 @@
 PATCHLIST="arctic-bugfix caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change citytools_changes client-fixes current_research_cost diplchanges nosave lake-fix goto_1 goto_attack1 goto_fix_1 goto_fix_2 govt-fix map-settings map_size_change metachange metapatch1 middle_click_info noquitidle orders_aborted orders_invalid2 savegame-support text_fixes tileset_hack turnchange unit_dif unithand-change2 webclient-ai-attitude freeciv-svn-webclient-changes network-rewrite-1 city_naming_logic_fix fcnet_packets misc_devversion_sync capstr"
 
 apply_patch() {
-  patch -u -p1 -d freeciv.upstream < patches/$1.diff
+  patch -u -p1 -d freeciv < patches/$1.diff
 }
 
 for patch in $PATCHLIST

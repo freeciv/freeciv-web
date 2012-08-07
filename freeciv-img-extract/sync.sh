80d8e66df1a021b05425b8478f2e1a0173cd1279
Marko Lindqvist <cazfi74@gmail.com>
2012-08-07 22:36:58 +0300

    Removed generated files from git.

--- freeciv-img-extract/sync.sh
+++ freeciv-img-extract/sync.sh
@@ -1,5 +1,6 @@
 python img-extract.py && \
 pngcrush pre-freeciv-web-tileset.png freeciv-web-tileset.png && \
+mkdir -p ../freeciv-web/src/main/webapp/tileset &&
 cp freeciv-web-tileset.png ../freeciv-web/src/main/webapp/tileset/ && \
 cp freeciv-web-tileset.js ../freeciv-web/src/main/webapp/javascript/ 
 cp tiles/*.png ../freeciv-web/src/main/webapp/tiles/

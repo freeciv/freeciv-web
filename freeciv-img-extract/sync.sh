21a39061605062f0d7cc8a2c7c333832e3cdd997
Marko Lindqvist <cazfi74@gmail.com>
2012-08-28 01:31:11 +0300

    Create tiles directory if it's missing.

--- freeciv-img-extract/sync.sh
+++ freeciv-img-extract/sync.sh
@@ -1,6 +1,7 @@
-python img-extract.py && \
-pngcrush pre-freeciv-web-tileset.png freeciv-web-tileset.png && \
+python img-extract.py &&
+pngcrush pre-freeciv-web-tileset.png freeciv-web-tileset.png &&
 mkdir -p ../freeciv-web/src/main/webapp/tileset &&
-cp freeciv-web-tileset.png ../freeciv-web/src/main/webapp/tileset/ && \
-cp freeciv-web-tileset.js ../freeciv-web/src/main/webapp/javascript/ 
+cp freeciv-web-tileset.png ../freeciv-web/src/main/webapp/tileset/ &&
+cp freeciv-web-tileset.js ../freeciv-web/src/main/webapp/javascript/ &&
+mkdir -p ../freeciv-web/src/main/webapp/tiles &&
 cp tiles/*.png ../freeciv-web/src/main/webapp/tiles/

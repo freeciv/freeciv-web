dff12ed0af1f6bbaf8542f33c697c269960a393a
Marko Lindqvist <cazfi74@gmail.com>
2012-08-28 04:23:42 +0300

    Updated freeciv server to revision 21577.

--- freeciv-proxy/civcom.py
+++ freeciv-proxy/civcom.py
@@ -22,7 +22,7 @@ import time
 
 HOST = 'localhost';
 MAX_LEN_PACKET = 48;
-VERSION = "+Freeciv.Web.Devel-2.5-2012.Aug.08";  # Must be kept in sync with Freeciv server.
+VERSION = "+Freeciv.Web.Devel-2.5-2012.Aug.28";  # Must be kept in sync with Freeciv server.
 VER_INFO = "-dev";
 logger = logging.getLogger("freeciv-proxy");
 

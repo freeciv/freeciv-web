b0a777ff4f7453163e11ef05e949f6b2428ecc41
Marko Lindqvist <cazfi74@gmail.com>
2013-03-16 14:17:24 +0200

    Updated resin to version 4.0.35

--- freeciv-web/build.sh
+++ freeciv-web/build.sh
@@ -1,7 +1,9 @@
 
+RESINVER="4.0.35"
+
 ROOTDIR="$(pwd)/../.."
 
 cd src/main/webapp/ &&
 sh compress-js.sh &&
 cd ../../../ &&
-mvn install && cp target/freeciv-web.war "${ROOTDIR}/resin-4.0.29/webapps/ROOT.war" && cd "${ROOTDIR}/resin-4.0.29/webapps/" && rm -rf ROOT && unzip ROOT.war -d ROOT
+mvn install && cp target/freeciv-web.war "${ROOTDIR}/resin-${RESINVER}/webapps/ROOT.war" && cd "${ROOTDIR}/resin-${RESINVER}/webapps/" && rm -rf ROOT && unzip ROOT.war -d ROOT

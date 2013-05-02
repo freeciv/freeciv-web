89e5189b64b9696de9a8088a9a526ff5e8297383
Andreas Rødal <andreas.rosdal@gmail.com>
2013-05-02 11:10:26 -0700

    move ga tracking code to head.

--- freeciv-web/src/main/webapp/freecivmetaserve/metaserver.php
+++ freeciv-web/src/main/webapp/freecivmetaserve/metaserver.php
@@ -352,6 +352,16 @@ if ( isset($port) ) {
 <script type="text/javascript" src="/javascript/jquery-ui-1.10.2.custom.min.js"></script>
 
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40584174-1', 'freeciv.org');
+  ga('send', 'pageview');
+      
+</script>
 
 </head>
 <body link="white" vlink="white" alink="white" bgcolor="black">
@@ -632,16 +642,6 @@ Copyright &copy; 2008-2013 Freeciv-web
 </div>
 
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-40584174-1', 'freeciv.org');
-  ga('send', 'pageview');
-      
-</script>
 
 <script>
 		$( ".button").button();

d2c9306a4c107ef38b2d192f4c16acf7d6a28190
Marko Lindqvist <cazfi74@gmail.com>
2013-05-12 08:10:55 +0300

    Dynamically get freeciv-web directory instead of hardcoding it.

--- scripts/start-freeciv-web.sh
+++ scripts/start-freeciv-web.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Startup script for running all processes of Freeciv-web
 
+SCRIPT_DIR="$(dirname "$0")"
+export FREECIV_WEB_DIR="${SCRIPT_DIR}/../.."
+
 echo "Starting up Freeciv-web: nginx, resin, publite2, freeciv-proxy."
 
 # 1. nginx
@@ -18,25 +21,21 @@ fi
 
 # 2. Resin
 echo "Starting up Resin" && \
-~/freeciv-build/resin/bin/resin.sh start && \
+${FREECIV_WEB_DIR}/resin/bin/resin.sh start && \
 echo "Resin started" && \
 sleep 20 && \
 
 #3. publite2
 echo "Starting publite2" && \
-cd ~/freeciv-build/freeciv-web/publite2/ && \
+cd ${FREECIV_WEB_DIR}/freeciv-web/publite2/ && \
 sh run.sh && \
 echo "Publite2 started" && \
 
 #4. freeciv-proxy
 echo "Starting freeciv-proxy" && \
-cd  ~/freeciv-build/freeciv-web/freeciv-proxy/ && \
+cd  ${FREECIV_WEB_DIR}/freeciv-web/freeciv-proxy/ && \
 sh run.sh & \
 echo "freeciv-proxy started" &&\
 echo "Will sleep for 30 seconds, then do a status test..." && \
 sleep 30 && \
 sh ./status-freeciv-web.sh
-
-
-
-

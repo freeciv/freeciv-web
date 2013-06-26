8e3130b49169c37cf06223ad43cc10ebc2f69b5d
Andreas Røsdal <andreas.rosdal@gmail.com>
2013-06-26 08:23:35 -0700

    Add fallback method to dashed line rendering.

--- freeciv-web/src/main/webapp/javascript/mapview.js
+++ freeciv-web/src/main/webapp/javascript/mapview.js
@@ -96,6 +96,31 @@ function init_mapview()
   
   init_sprites();
 
+  if (!(dashedSupport || mozDashSupport) 
+     && window.CanvasRenderingContext2D 
+     && CanvasRenderingContext2D.prototype.lineTo) {
+    CanvasRenderingContext2D.prototype.dashedLine = function(x,y,x2,y2,dashArray){
+    if (!dashArray) dashArray=[10,5];
+      var dashCount = dashArray.length;
+      this.moveTo(x, y);
+      var dx = (x2-x), dy = (y2-y);
+      var slope = dy/dx;
+      var distRemaining = Math.sqrt( dx*dx + dy*dy );
+      var dashIndex=0, draw=true;
+      while (distRemaining>=0.1 && dashIndex<10000){
+        var dashLength = dashArray[dashIndex++%dashCount];
+        if (dashLength > distRemaining) dashLength = distRemaining;
+          var xStep = Math.sqrt( dashLength*dashLength / (1 + slope*slope) );
+          x += xStep
+          y += slope*xStep;
+          this[draw ? 'lineTo' : 'moveTo'](x,y);
+          distRemaining -= dashLength;
+          draw = !draw;
+      }
+      this.moveTo(0,0);
+    }
+  }
+
 
 }
 
@@ -374,17 +399,33 @@ function mapview_put_border_line(pcanvas, dir, color, canvas_x, canvas_y) {
 
   pcanvas.beginPath();
   if (dir == DIR8_NORTH) {
-    pcanvas.moveTo(x, y - 2, x + (tileset_tile_width / 2));
-    pcanvas.lineTo(x + (tileset_tile_width / 2),  y + (tileset_tile_height / 2) - 2);
+    if (dashedSupport || mozDashSupport) {
+      pcanvas.moveTo(x, y - 2, x + (tileset_tile_width / 2));
+      pcanvas.lineTo(x + (tileset_tile_width / 2),  y + (tileset_tile_height / 2) - 2);
+    } else {
+      pcanvas.dashedLine(x, y - 2, x + (tileset_tile_width / 2),  y + (tileset_tile_height / 2) - 2, [4, 4]);
+    }
   } else if (dir == DIR8_EAST) {
-    pcanvas.moveTo(x - 3, y + tileset_tile_height - 3);
-    pcanvas.lineTo(x + (tileset_tile_width / 2) - 3,  y + (tileset_tile_height / 2) - 3);
+    if (dashedSupport || mozDashSupport) {
+      pcanvas.moveTo(x - 3, y + tileset_tile_height - 3);
+      pcanvas.lineTo(x + (tileset_tile_width / 2) - 3,  y + (tileset_tile_height / 2) - 3);
+    } else {
+      pcanvas.dashedLine(x - 3, y + tileset_tile_height - 3, x + (tileset_tile_width / 2) - 3,  y + (tileset_tile_height / 2) - 3, [4, 4]);
+    }
   } else if (dir == DIR8_SOUTH) {
-    pcanvas.moveTo(x - (tileset_tile_width / 2) + 3, y + (tileset_tile_height / 2) - 3);
-    pcanvas.lineTo(x + 3,  y + tileset_tile_height - 3);
+    if (dashedSupport || mozDashSupport) {
+      pcanvas.moveTo(x - (tileset_tile_width / 2) + 3, y + (tileset_tile_height / 2) - 3);
+      pcanvas.lineTo(x + 3,  y + tileset_tile_height - 3);
+    } else {
+      pcanvas.dashedLine(x - (tileset_tile_width / 2) + 3, y + (tileset_tile_height / 2) - 3, x + 3,  y + tileset_tile_height - 3, [4, 4]);
+    }
   } else if (dir == DIR8_WEST) {
-    pcanvas.moveTo(x - (tileset_tile_width / 2) + 3, y + (tileset_tile_height / 2) - 3);
-    pcanvas.lineTo(x + 3,  y - 3);
+    if (dashedSupport || mozDashSupport) {
+      pcanvas.moveTo(x - (tileset_tile_width / 2) + 3, y + (tileset_tile_height / 2) - 3);
+      pcanvas.lineTo(x + 3,  y - 3);
+    } else {
+      pcanvas.dashedLine(x - (tileset_tile_width / 2) + 3, y + (tileset_tile_height / 2) - 3, x + 3,  y - 3, [4, 4]);
+    }
   }
   pcanvas.closePath();
   pcanvas.stroke();

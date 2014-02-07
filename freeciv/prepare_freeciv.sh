c006c118334fab960f2987f5747a0867cbf4ea03
Marko Lindqvist <cazfi74@gmail.com>
2014-02-08 00:52:17 +0200

    Added single script to get, patch, and build freeciv server
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

---
+++ freeciv/prepare_freeciv.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if test "x$(dirname $0)" != "x." ; then
+  echo "Currently this script must be run on its own directory" >&2
+  exit 1
+fi
+
+# Remove old version
+rm -Rf freeciv
+
+. ./version.txt
+
+if ! svn export svn://svn.gna.org/svn/freeciv/$FCBRANCH -r $FCREV freeciv ; then
+  echo "Svn export failed" >&2
+  exit 1
+fi
+
+if ! ./apply_patches.sh ; then
+  echo "Patching failed" >&2
+  exit 1
+fi
+
+( cd freeciv
+
+  ./autogen.sh --enable-fcweb --disable-nls --disable-debug && make
+)

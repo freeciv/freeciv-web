045be06bcd75319244b7d27096613e68a114c79e
Marko Lindqvist <cazfi74@gmail.com>
2014-02-16 05:34:20 +0200

    Update freeciv server to r24476
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# - maphand_ch should not add concat_tile_activity_text() (patch #4484)
 # - metatype will be replaced by patch #4485
 
 PATCHLIST="caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes current_research_cost diplchanges goto_1 goto_attack1 goto_fix_1 goto_fix_2 govt-fix map-settings map_size_change metachange metatype metasrvflag packets orders_aborted orders_invalid2 text_fixes turnchange unithand-change2 webclient-ai-attitude freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync specs_format scenario_ruleset savegame savegame2 maphand_ch serverside_extra_assign"

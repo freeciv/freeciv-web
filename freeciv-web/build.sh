633f0a74c1fd05d444102fac62f87049ced9967b
Andreas Røsdal <andreas.rosdal@gmail.com>
2014-02-24 23:06:54 +0100

    Merge compress-js.sh into build.sh, move compiler.jar and clean up freeciv-web build.sh script.

--- freeciv-web/build.sh
+++ freeciv-web/build.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+
+# bundle and compress Freeciv-web Javascript using Google Closure Compiler
+# then build Freeciv-web using Maven.
 
 ROOTDIR="$(pwd)/.."
 
-cd src/main/webapp/ &&
-sh compress-js.sh &&
-cd ../../../ &&
-mvn install && cp target/freeciv-web.war "${ROOTDIR}/resin/webapps/ROOT.war" && cd "${ROOTDIR}/resin/webapps/" && rm -rf ROOT && unzip ROOT.war -d ROOT
+rm ./src/main/webapp/javascript-compressed/webclient.js -rf
+kommandoen="java -jar ./src/main/webapp/WEB-INF/compiler.jar --create_source_map freeciv-web-source-map "
+for f in `ls src/main/webapp/javascript/*.js`
+do
+  kommandoen="$kommandoen --js=$f"
+done
+
+kommandoen="$kommandoen --js_output_file=src/main/webapp/javascript-compressed/webclient.js"
+
+# run compressor and then build using Maven
+$kommandoen && mvn install && cp target/freeciv-web.war "${ROOTDIR}/resin/webapps/ROOT.war"

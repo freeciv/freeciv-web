4223ab9f6b6e1bd95fcd804a4dbb5c1fb8d5a19e
Andreas Røsdal <andreas.rosdal@gmail.com>
2014-02-27 19:57:51 +0100

    Use scenarios from Freeciv SVN repository instead of bundled and modified Freeciv-web scenarios, as they now work correctly in Freeciv SVN.

--- publite2/run.sh
+++ publite2/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 export LANG=C
-FREECIV_SAVE_PATH="~/freeciv-build/resin/webapps/ROOT/savegames/"
+
+if [ -d "/vagrant/" ]; then
+  FREECIV_SAVE_PATH="/vagrant/resin/webapps/ROOT/savegames/"
+else
+  FREECIV_SAVE_PATH="~/freeciv-build/freeciv-web/resin/webapps/ROOT/savegames/"
+fi
 export FREECIV_SAVE_PATH
+
 
 nohup python3.3 publite2.py || tail -5 nohup.out && sleep 5  &
--- scripts/sync-js-hand.sh
+++ scripts/sync-js-hand.sh
@@ -3,6 +3,5 @@
 python generate_js_hand/generate_js_hand.py && \
 cp packhand_gen.js ../freeciv-web/src/main/webapp/javascript/ && \
 cp packets.js ../freeciv-web/src/main/webapp/javascript/ && \
-cp ../freeciv/scenarios/*.sav ../freeciv-web/src/main/webapp/savegames/ && \
-cp ../freeciv/freeciv/data/scenarios/tutorial.sav ../freeciv-web/src/main/webapp/savegames/ && \
+cp ../freeciv/freeciv/data/scenarios/*.sav ../freeciv-web/src/main/webapp/savegames/ && \
 echo "done!"

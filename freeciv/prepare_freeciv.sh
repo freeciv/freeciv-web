777e0a293d9a1af29f87202850fc93a264826dc7
Marko Lindqvist <cazfi74@gmail.com>
2014-03-07 23:38:14 +0200

    Do not build freeciv-manual
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/prepare_freeciv.sh
+++ freeciv/prepare_freeciv.sh
@@ -22,5 +22,5 @@ fi
 
 ( cd freeciv
 
-  ./autogen.sh --enable-fcweb --disable-nls --disable-debug --enable-fcmp=no && make
+  ./autogen.sh --enable-fcweb --disable-nls --disable-debug --disable-fcmp --disable-freeciv-manual && make
 )

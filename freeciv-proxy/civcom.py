ee0daee38a97e497550112590c2e7f833e1a187c
Andreas Røsdal <andreas.rosdal@gmail.com>
2014-03-17 19:10:55 +0100

    Run autopep8 on Freeciv-proxy. Add session id to civcom key.

--- freeciv-proxy/civcom.py
+++ freeciv-proxy/civcom.py
@@ -1,7 +1,7 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
-''' 
- Freeciv - Copyright (C) 2009-2013 - Andreas Røsdal   andrearo@pvv.ntnu.no
+'''
+ Freeciv - Copyright (C) 2009-2014 - Andreas RÃ¸sdal   andrearo@pvv.ntnu.no
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
@@ -16,165 +16,181 @@
 import socket
 import select
 from struct import *
-from threading import Thread;
+from threading import Thread
 import logging
 import time
 
-HOST = '127.0.0.1';
-logger = logging.getLogger("freeciv-proxy");
+HOST = '127.0.0.1'
+logger = logging.getLogger("freeciv-proxy")
 
-# The CivCom handles communication between freeciv-proxy and the Freeciv C server.
+# The CivCom handles communication between freeciv-proxy and the Freeciv C
+# server.
+
+
 class CivCom(Thread):
 
-  def __init__ (self, username, civserverport, civwebserver):
-    Thread.__init__(self)
-    self.socket = None;
-    self.username = username;
-    self.civserverport = civserverport;
-    self.key = username + str(civserverport);
-    self.send_buffer = [];
-    self.connect_time = time.time();
-    self.civserver_messages = [];
-    self.stopped = False;
-    self.packet_size = -1;
-    self.net_buf = bytearray(0);
-    self.header_buf = bytearray(0);
-    self.daemon = True;
-    self.civwebserver = civwebserver;
+    def __init__(self, username, civserverport, civwebserver):
+        Thread.__init__(self)
+        self.socket = None
+        self.username = username
+        self.civserverport = civserverport
+        self.key = username + str(civserverport)
+        self.send_buffer = []
+        self.connect_time = time.time()
+        self.civserver_messages = []
+        self.stopped = False
+        self.packet_size = -1
+        self.net_buf = bytearray(0)
+        self.header_buf = bytearray(0)
+        self.daemon = True
+        self.civwebserver = civwebserver
 
-  def run(self):
-    #setup connection to civserver
-    if (logger.isEnabledFor(logging.INFO)):
-      logger.info("Start connection to civserver for " + self.username 
-                  + " from IP " + self.civwebserver.ip)
-    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.socket.setblocking(True);
-    self.socket.settimeout(2);
-    try:
-        self.socket.connect((HOST, self.civserverport))
-        self.socket.settimeout(0.01);
-    except socket.error as reason:
-      self.send_error_to_client("Proxy unable to connect to civserver. Error: %s" % (reason));
-      return;
+    def run(self):
+        # setup connection to civserver
+        if (logger.isEnabledFor(logging.INFO)):
+            logger.info("Start connection to civserver for " + self.username
+                        + " from IP " + self.civwebserver.ip)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setblocking(True)
+        self.socket.settimeout(2)
+        try:
+            self.socket.connect((HOST, self.civserverport))
+            self.socket.settimeout(0.01)
+        except socket.error as reason:
+            self.send_error_to_client(
+                "Proxy unable to connect to civserver. Error: %s" %
+                (reason))
+            return
 
-    # send initial login packet to civserver
-    self.civserver_messages = [self.civwebserver.loginpacket];
-    self.send_packets_to_civserver();
+        # send initial login packet to civserver
+        self.civserver_messages = [self.civwebserver.loginpacket]
+        self.send_packets_to_civserver()
 
-    #receive packets from server
-    while 1:
-      packet = self.read_from_connection(); 
+        # receive packets from server
+        while True:
+            packet = self.read_from_connection()
 
-      if (self.stopped):
-        return;
-              
-      if (packet != None): 
-        self.net_buf += packet;
+            if (self.stopped):
+                return
 
-        if (len(self.net_buf) == self.packet_size and self.net_buf[-1] == 0):
-          # valid packet received from freeciv server, send it to client.
-          self.send_buffer_append(self.net_buf[:-1]);
-          self.packet_size = -1;
-          self.net_buf = bytearray(0);
-          continue;
+            if (packet is not None):
+                self.net_buf += packet
 
+                if (len(self.net_buf) == self.packet_size and self.net_buf[-1] == 0):
+                    # valid packet received from freeciv server, send it to
+                    # client.
+                    self.send_buffer_append(self.net_buf[:-1])
+                    self.packet_size = -1
+                    self.net_buf = bytearray(0)
+                    continue
 
-      time.sleep(0.01); #prevent max CPU usage in case of error
+            time.sleep(0.01)
+            # prevent max CPU usage in case of error
 
-  def read_from_connection(self):
-    try:
-      if (self.socket != None and not self.stopped):    
-        if (self.packet_size == -1):
-          self.header_buf += self.socket.recv(4 - len(self.header_buf));
-          if (len(self.header_buf) == 0): 
-            self.close_connection();
-            return None;
-          if (len(self.header_buf) == 4):
-            header_pck = unpack('>HH', self.header_buf);
-            self.header_buf = bytearray(0);
-            self.packet_size = header_pck[0] - 4;
-            if (self.packet_size <= 0 or self.packet_size > 32767): 
-              logger.error("Invalid packet size.");
-          else:
-            # complete header not read yet. return now, and read the rest next time.
-            return None;
+    def read_from_connection(self):
+        try:
+            if (self.socket is not None and not self.stopped):
+                if (self.packet_size == -1):
+                    self.header_buf += self.socket.recv(4 -
+                                                        len(self.header_buf))
+                    if (len(self.header_buf) == 0):
+                        self.close_connection()
+                        return None
+                    if (len(self.header_buf) == 4):
+                        header_pck = unpack('>HH', self.header_buf)
+                        self.header_buf = bytearray(0)
+                        self.packet_size = header_pck[0] - 4
+                        if (self.packet_size <= 0 or self.packet_size > 32767):
+                            logger.error("Invalid packet size.")
+                    else:
+                        # complete header not read yet. return now, and read
+                        # the rest next time.
+                        return None
 
-      if (self.socket != None and self.net_buf != None and self.packet_size > 0):
-        data = self.socket.recv(self.packet_size - len(self.net_buf));
-        if (len(data) == 0):
-          self.close_connection();
-          return None;
+            if (self.socket is not None and self.net_buf is not None and self.packet_size > 0):
+                data = self.socket.recv(self.packet_size - len(self.net_buf))
+                if (len(data) == 0):
+                    self.close_connection()
+                    return None
 
-        return data;
-    except socket.timeout: 
-      self.send_packets_to_client();
-      self.send_packets_to_civserver();
-      return None;
-    except OSError:
-      return None;
+                return data
+        except socket.timeout:
+            self.send_packets_to_client()
+            self.send_packets_to_civserver()
+            return None
+        except OSError:
+            return None
 
-  
-  def close_connection(self):
-    if (logger.isEnabledFor(logging.INFO)):
-      logger.info("Server connection closed. Removing civcom thread for " + self.username);
-    
-    if (hasattr(self.civwebserver, "civcoms") and self.key in list(self.civwebserver.civcoms.keys())):
-      del self.civwebserver.civcoms[self.key];
-    
-    if (self.socket != None):
-      self.socket.close();
-      self.socket = None; 
+    def close_connection(self):
+        if (logger.isEnabledFor(logging.INFO)):
+            logger.info(
+                "Server connection closed. Removing civcom thread for " +
+                self.username)
 
-    self.stopped = True;
-    
+        if (hasattr(self.civwebserver, "civcoms") and self.key in list(self.civwebserver.civcoms.keys())):
+            del self.civwebserver.civcoms[self.key]
 
-  # queue messages to be sent to client. 
-  def send_buffer_append(self, data):
-    try:
-      self.send_buffer.append(data.decode(encoding="utf-8", errors="ignore"));
-    except UnicodeDecodeError:
-      if (logger.isEnabledFor(logging.ERROR)):
-        logger.error("Unable to decode string from civcom socket, for user: " + self.username);
-      return;
+        if (self.socket is not None):
+            self.socket.close()
+            self.socket = None
 
-  # sends packets to client (WebSockets client / browser)
-  def send_packets_to_client(self):
-    packet = self.get_client_result_string();
-    if (packet != None and self.civwebserver != None):
-      self.civwebserver.write_message(packet);
+        self.stopped = True
 
+    # queue messages to be sent to client.
+    def send_buffer_append(self, data):
+        try:
+            self.send_buffer.append(
+                data.decode(
+                    encoding="utf-8",
+                    errors="ignore"))
+        except UnicodeDecodeError:
+            if (logger.isEnabledFor(logging.ERROR)):
+                logger.error(
+                    "Unable to decode string from civcom socket, for user: " +
+                    self.username)
+            return
 
-  def get_client_result_string(self):
-    result = "";
-    try:
-      if len(self.send_buffer) > 0:
-        result = "[" + ",".join(self.send_buffer) + "]";
-      else:
-        result = None;
-    finally:
-      del self.send_buffer[:];
-    return result;
+    # sends packets to client (WebSockets client / browser)
+    def send_packets_to_client(self):
+        packet = self.get_client_result_string()
+        if (packet is not None and self.civwebserver is not None):
+            self.civwebserver.write_message(packet)
 
-  def send_error_to_client(self, message):
-    if (logger.isEnabledFor(logging.ERROR)):
-      logger.error(message);
-    self.send_buffer_append(("{\"pid\":18,\"message\":\"" + message + "\"}").encode("utf-8"));
- 
+    def get_client_result_string(self):
+        result = ""
+        try:
+            if len(self.send_buffer) > 0:
+                result = "[" + ",".join(self.send_buffer) + "]"
+            else:
+                result = None
+        finally:
+            del self.send_buffer[:]
+        return result
 
-  # Send packets from freeciv-proxy to civserver   
-  def send_packets_to_civserver(self):
-    if (self.civserver_messages is None or self.socket is None): return;
+    def send_error_to_client(self, message):
+        if (logger.isEnabledFor(logging.ERROR)):
+            logger.error(message)
+        self.send_buffer_append(
+            ("{\"pid\":18,\"message\":\"" + message + "\"}").encode("utf-8"))
 
-    try:
-      for net_message in self.civserver_messages:
-        header = pack('>HH', len(net_message), 0);
-        self.socket.sendall(header + net_message.encode('utf-8') + b'\0');
-    except:
-      self.send_error_to_client("Proxy unable to communicate with civserver on port " 
-                                + str(self.civserverport));
-    finally:
-      self.civserver_messages = [];
+    # Send packets from freeciv-proxy to civserver
+    def send_packets_to_civserver(self):
+        if (self.civserver_messages is None or self.socket is None):
+            return
 
-  # queue message for the civserver
-  def queue_to_civserver(self, message):
-    self.civserver_messages.append(message);
+        try:
+            for net_message in self.civserver_messages:
+                header = pack('>HH', len(net_message), 0)
+                self.socket.sendall(
+                    header +
+                    net_message.encode('utf-8') +
+                    b'\0')
+        except:
+            self.send_error_to_client(
+                "Proxy unable to communicate with civserver on port " + str(self.civserverport))
+        finally:
+            self.civserver_messages = []
+
+    # queue message for the civserver
+    def queue_to_civserver(self, message):
+        self.civserver_messages.append(message)
--- freeciv-proxy/debugging.py
+++ freeciv-proxy/debugging.py
@@ -1,7 +1,7 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
-''' 
- Freeciv - Copyright (C) 2009-2013 - Andreas Røsdal   andrearo@pvv.ntnu.no
+'''
+ Freeciv - Copyright (C) 2009-2014 - Andreas Røsdal   andrearo@pvv.ntnu.no
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
@@ -17,74 +17,81 @@
 import sys
 import traceback
 from time import gmtime, strftime
-import os, platform
+import os
+import platform
 import time
 from tornado import version as tornado_version
 
 _proc_status = '/proc/%d/status' % os.getpid()
 
-_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
-          'KB': 1024.0, 'MB': 1024.0*1024.0}
+_scale = {'kB': 1024.0, 'mB': 1024.0 * 1024.0,
+          'KB': 1024.0, 'MB': 1024.0 * 1024.0}
 
 startTime = time.time()
 
+
 def get_debug_info(civcoms):
-  code = "<html><head><meta http-equiv=\"refresh\" content=\"20\"></head><body><h2>Freeciv WebSocket Proxy Status</h2>"
-  code += "<font color=\"green\">Process status: OK</font><br>";
+    code = "<html><head><meta http-equiv=\"refresh\" content=\"20\"></head><body><h2>Freeciv WebSocket Proxy Status</h2>"
+    code += "<font color=\"green\">Process status: OK</font><br>"
 
-  code += "<h3>Process Uptime: " + str(int(time.time() - startTime)) + " s.</h3>";
-  current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime());
-  code += "<b>Current time:</b><br> " + current_time;
+    code += "<h3>Process Uptime: " + \
+        str(int(time.time() - startTime)) + " s.</h3>"
+    current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    code += "<b>Current time:</b><br> " + current_time
 
-  code += ("<br><br>Python version: %s %s (%s)<br>" % (
-      platform.python_implementation(),
-      platform.python_version(),
-      platform.python_build()[0],
-  ));
+    code += ("<br><br>Python version: %s %s (%s)<br>" % (
+        platform.python_implementation(),
+        platform.python_version(),
+        platform.python_build()[0],
+    ))
 
-  cpu = ' '.join(platform.processor().split())
-  code += ("Platform: %s %s on '%s' <br>" % (
-      platform.machine(),
-      platform.system(),
-      cpu));
+    cpu = ' '.join(platform.processor().split())
+    code += ("Platform: %s %s on '%s' <br>" % (
+        platform.machine(),
+        platform.system(),
+        cpu))
 
-  code += ("Tornado version %s <br>" % (tornado_version));
+    code += ("Tornado version %s <br>" % (tornado_version))
 
-  try:
-    f = open("/proc/loadavg");
-    contents = f.read();
-    code += "Load average: " + contents;
-    f.close()
-  except:
-    print("Cannot open uptime file: /proc/uptime");
+    try:
+        f = open("/proc/loadavg")
+        contents = f.read()
+        code += "Load average: " + contents
+        f.close()
+    except:
+        print("Cannot open uptime file: /proc/uptime")
 
-  try:    
-    code += "<h3>Memory usage:</h3>";
-    code += "Memory: " + str(memory()/1048576) + " MB <br>";
-    code += "Resident: " + str(resident()/1048576) + " MB <br>";
-    code += "Stacksize: " + str(stacksize()/1048576) + " MB <br>";
+    try:
+        code += "<h3>Memory usage:</h3>"
+        code += "Memory: " + str(memory() / 1048576) + " MB <br>"
+        code += "Resident: " + str(resident() / 1048576) + " MB <br>"
+        code += "Stacksize: " + str(stacksize() / 1048576) + " MB <br>"
 
-    code += ("<h3>Logged in users  (count %i) :</h3>" % len(civcoms));
-    for key in list(civcoms.keys()):
-      code += ("username: <b>%s</b> <br>IP:%s <br>Civserver: (%d)<br>Connect time: %d<br><br>" % (civcoms[key].username, civcoms[key].civwebserver.ip, civcoms[key].civserverport,   time.time() - civcoms[key].connect_time));
-       
+        code += ("<h3>Logged in users  (count %i) :</h3>" % len(civcoms))
+        for key in list(civcoms.keys()):
+            code += (
+                "username: <b>%s</b> <br>IP:%s <br>Civserver: (%d)<br>Connect time: %d<br><br>" %
+                (civcoms[key].username,
+                 civcoms[key].civwebserver.ip,
+                    civcoms[key].civserverport,
+                    time.time() -
+                    civcoms[key].connect_time))
 
- 
-    code += "<h3>Thread dumps:</h3>";
+        code += "<h3>Thread dumps:</h3>"
 
-    for threadId, stack in list(sys._current_frames().items()):
-        code += ("<br><br><b><u># ThreadID: %s</u></b><br>" % threadId)
-        for filename, lineno, name, line in traceback.extract_stack(stack):
-            code += ('File: "%s", line %d, in %s: ' % (filename, lineno, name))
-            if line:
-                code += (" <b>%s</b> <br>" % (line.strip()))
+        for threadId, stack in list(sys._current_frames().items()):
+            code += ("<br><br><b><u># ThreadID: %s</u></b><br>" % threadId)
+            for filename, lineno, name, line in traceback.extract_stack(stack):
+                code += ('File: "%s", line %d, in %s: ' %
+                         (filename, lineno, name))
+                if line:
+                    code += (" <b>%s</b> <br>" % (line.strip()))
 
+    except:
+        print(("Unexpected error:" + str(sys.exc_info()[0])))
+        raise
 
-  except:
-    print(("Unexpected error:" + str(sys.exc_info()[0])));
-    raise
-
-  return code;
+    return code
 
 
 def _VmB(VmKey):

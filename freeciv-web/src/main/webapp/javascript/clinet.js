637f5ad66b45cda3c5fbf341e49d019ca2918aaf
Andreas Røsdal <andreas.rosdal@gmail.com>
2014-05-21 22:45:05 +0200

    Update to Freeciv svn r24916.

--- freeciv-web/src/main/webapp/javascript/clinet.js
+++ freeciv-web/src/main/webapp/javascript/clinet.js
@@ -19,7 +19,7 @@ var isWorking = false;
 var clinet_last_send = 0;
 var debug_client_speed_list = [];
 
-var freeciv_version = "+Freeciv.Web.Devel-2.6-2014.May.17";
+var freeciv_version = "+Freeciv.Web.Devel-2.6-2014.May.21";
 
 var ws = null;
 var civserverport = null;

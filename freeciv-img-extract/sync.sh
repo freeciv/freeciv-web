cc78dc9399eb4107c3423b28ab6d97a11f45f6b6
Andreas Røsdal <andreas.rosdal@gmail.com>
2014-05-28 23:07:05 +0200

    Convert flags in svg files to png, and show them in nation dialog.

--- freeciv-img-extract/sync.sh
+++ freeciv-img-extract/sync.sh
@@ -6,4 +6,11 @@ mkdir -p ../freeciv-web/src/main/webapp/tileset &&
 cp freeciv-web-tileset-0.png ../freeciv-web/src/main/webapp/tileset/ &&
 cp freeciv-web-tileset-1.png ../freeciv-web/src/main/webapp/tileset/ &&
 cp freeciv-web-tileset.js ../freeciv-web/src/main/webapp/javascript/ && 
+echo "converting flag svg files to png..." && 
+mkdir -p ../freeciv-web/src/main/webapp/images/flags/ &&
+(for X in `find ../freeciv/freeciv/data/flags/*.svg` 
+do
+ convert -density 80 -resize 180 $X ${X/.svg/-web.png}
+done) &&
+mv ../freeciv/freeciv/data/flags/*-web.png ../freeciv-web/src/main/webapp/images/flags/ &&	
 echo "Freeciv-img-extract done." || echo "Freeciv-img-extract failed!"

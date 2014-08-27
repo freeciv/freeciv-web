bdfd1caf881dacb0b0d7847cd2f41bf6f093455e
Marko Lindqvist <cazfi74@gmail.com>
2014-08-27 04:34:47 +0300

    Update freeciv server to r26056
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv-web/src/main/webapp/javascript/clinet.js
+++ freeciv-web/src/main/webapp/javascript/clinet.js
@@ -19,7 +19,7 @@ var isWorking = false;
 var clinet_last_send = 0;
 var debug_client_speed_list = [];
 
-var freeciv_version = "+Freeciv.Web.Devel-2.6-2014.Aug.12";
+var freeciv_version = "+Freeciv.Web.Devel-2.6-2014.Aug.27";
 
 var ws = null;
 var civserverport = null;

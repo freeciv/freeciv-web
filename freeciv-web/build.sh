3ae8584a497db73b07bd15383b629d1a6a2dbe0f
Andreas Røsdal <andreas.rosdal@gmail.com>
2014-09-11 22:10:08 +0200

    Update mysql-connector-java to version 5.1.32. Change build.sh to copy local.php.dist to that the metaserver works on Vagrant.

--- freeciv-web/build.sh
+++ freeciv-web/build.sh
@@ -2,5 +2,5 @@
 # builds Freeciv-web and copies the war file to resin.
 
 ROOTDIR="$(pwd)/.."
-
+cp src/main/webapp/meta/php_code/local.php.dist src/main/webapp/meta/php_code/local.php
 mvn install && cp target/freeciv-web.war "${ROOTDIR}/resin/webapps/ROOT.war"

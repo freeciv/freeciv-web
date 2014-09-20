4ff98c51ddd80edcf0f7b10c271cdd76b1e89df1
Andreas Røsdal <andreas.rosdal@gmail.com>
2014-09-20 17:36:24 +0200

    Sync with prod.

--- scripts/log-cleanup.sh
+++ scripts/log-cleanup.sh
@@ -3,7 +3,7 @@
 
 cp /dev/null /home/freeciv/freeciv-web/resin/log/jvm-app-0.log
 cp /dev/null /home/freeciv/freeciv-web/resin/log/access.log
-cp /dev/null /home/freeciv/freeciv-web/freeciv-web/logs/*.log
+find /home/freeciv/freeciv-web/freeciv-web/logs/*.log -exec cp /dev/null {} \;
 cp /dev/null /usr/local/nginx/logs/access.log
 cp /dev/null /usr/local/nginx/logs/error.log
 

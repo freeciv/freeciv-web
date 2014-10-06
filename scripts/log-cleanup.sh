c5b5941ebcb2142aad39fd6931ef433e77c6ecf8
Andreas Røsdal <andreas.rosdal@gmail.com>
2014-10-06 19:44:10 +0200

    Update paths to resin log files.

--- scripts/log-cleanup.sh
+++ scripts/log-cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # cleans up logs
 
-cp /dev/null /home/freeciv/freeciv-web/resin/log/jvm-app-0.log
-cp /dev/null /home/freeciv/freeciv-web/resin/log/access.log
+cp /dev/null /home/freeciv/freeciv-web/freeciv-web/resin/log/jvm-app-0.log
+cp /dev/null /home/freeciv/freeciv-web/freeciv-web/resin/log/access.log
 find /home/freeciv/freeciv-web/freeciv-web/logs/*.log -exec cp /dev/null {} \;
 cp /dev/null /usr/local/nginx/logs/access.log
 cp /dev/null /usr/local/nginx/logs/error.log

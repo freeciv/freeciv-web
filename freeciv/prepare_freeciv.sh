cc10107f6af749799647ad4c9278bfe78c8d3dfd
Marko Lindqvist <cazfi74@gmail.com>
2014-10-30 03:41:04 +0200

    Really do not build freeciv-manual
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/prepare_freeciv.sh
+++ freeciv/prepare_freeciv.sh
@@ -22,5 +22,5 @@ fi
 
 ( cd freeciv
 
-  ./autogen.sh --enable-fcweb --disable-nls --disable-debug --disable-fcmp --disable-freeciv-manual --disable-ruledit --enable-ai-static=classic,threaded && make
+  ./autogen.sh --enable-fcweb --disable-nls --disable-debug --disable-fcmp --without-freeciv-manual --disable-ruledit --enable-ai-static=classic,threaded && make
 )

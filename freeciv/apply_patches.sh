e7f3bb674655a14b8b35106b0be1c3f19749166d
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-03-22 20:21:41 +0100

    Let the player know when an order to move fails.
    The web client now use the orders system directly.
     - Stop removing error reports in the orders system.
     - Remove error reporting in the handling of the unit_move packet so the same error isn't reported twice.

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -4,7 +4,7 @@
 # revert_5729 reverts patch #5729 for now, someone needs to make that to work
 #    on freeciv-web.
 
-PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes revert_5729 goto_1 goto_attack1 goto_fix_1 goto_fix_2 govt-fix map-settings metachange orders_aborted orders_invalid2 text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame savegame2 maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader"
+PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes revert_5729 goto_1 goto_attack1 goto_fix_1 goto_fix_2 govt-fix map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame savegame2 maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

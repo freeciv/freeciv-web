cf5e1dcbde82f030ff4cda6a7aaf3e4878e585d7
Andreas Røsdal <andreas.rosdal@gmail.com>
2015-07-02 13:59:33 +0200

    Hide menu on frontpage on devices with small screens.

--- freeciv-web/src/main/webapp/javascript/menu.js
+++ freeciv-web/src/main/webapp/javascript/menu.js
@@ -10,6 +10,38 @@ $(document).ready(function() {
     $('#load-button').addClass("btn-success");
   }
 
+  if ($(window).width() <= 768) {
+    $(".nav").hide();
+  }
+
+    if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+      $("#chromews").hide();
+      $("#mozws").hide();
+      $("#windowsstore").hide();
+      $("#playws").show();
+    } else if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+      $("#chromews").show();
+      $("#mozws").hide();
+      $("#windowsstore").hide();
+      $("#playws").hide();
+    } else if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+      $("#mozws").show();
+      $("#chromews").hide();
+      $("#windowsstore").hide();
+      $("#playws").hide();
+    } else if (navigator.userAgent.toLowerCase().indexOf('trident') > -1) {
+      $("#mozws").hide();
+      $("#chromews").hide();
+      $("#windowsstore").show();
+      $("#playws").hide();
+    } else {
+      $("#mozws").hide();
+      $("#chromews").hide();
+      $("#windowsstore").hide();
+      $("#playws").hide();
+    }
+
+
 
 $.ajax({
   url: "/meta/fpinfo.php",

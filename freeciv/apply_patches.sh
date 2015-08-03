16a82f4c03a4284f073fff72f797de905e6ee0da
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-08-04 00:40:33 +0200

    Freeciv server: remove savegame2.patch
    Freeciv-web is currently using savegame3. A patch to savegame2 must therefore be unused.

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -8,7 +8,7 @@
 # dev_save_compat is Freeciv patch #6154.
 # action_order_savegame_compat is Freeciv patch #6171. It makes pre patch #6153 savegames work.
 
-PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame savegame2 maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix order_validation_feed_back action_order fix_unit_order_action_validation dev_save_compat action_order_savegame_compat"
+PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix order_validation_feed_back action_order fix_unit_order_action_validation dev_save_compat action_order_savegame_compat"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

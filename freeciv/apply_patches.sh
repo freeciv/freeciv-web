8b0732bfe95c9e4dfd8bcf8b980904a6248a29e0
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-08-05 17:43:58 +0200

    Update to Freeciv server r29306

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -2,11 +2,10 @@
 
 # activity_null_check is for Freeciv bug #22700.
 # city_traderoute_hotfix is for Freeciv bug #23708.
-# fix_unit_order_action_validation is Freeciv bug #23737.
 # dev_save_compat is Freeciv patch #6154.
 # action_order_savegame_compat is Freeciv patch #6171. It makes pre patch #6153 savegames work.
 
-PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix fix_unit_order_action_validation dev_save_compat action_order_savegame_compat"
+PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix dev_save_compat action_order_savegame_compat"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

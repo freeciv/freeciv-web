1982227fffa62701b7cc5bdcc3280f4648191ae4
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-08-24 20:48:49 +0200

    Stop claiming that occupied cities are unoccupied
    See bug #23813

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -2,8 +2,9 @@
 
 # activity_null_check is for Freeciv bug #22700.
 # city_traderoute_hotfix is for Freeciv bug #23708.
+# city_occupied_fix is for Freeciv bug #23813.
 
-PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix"
+PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix city_occupied_fix"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

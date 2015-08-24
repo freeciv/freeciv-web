356881ced093c3d23ee87902e35d6b7b46c6bc4c
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-08-24 14:06:51 +0200

    Update to Freeciv server r29459

--- freeciv-web/src/main/webapp/javascript/fc_types.js
+++ freeciv-web/src/main/webapp/javascript/fc_types.js
@@ -108,7 +108,10 @@ var VUT_MINMOVES = 31;
 var VUT_MINVETERAN = 32;
 var VUT_MINHP = 33;
 var VUT_AGE = 34
-var VUT_COUNT = 35;             /* Keep this last. */
+var VUT_NATIONGROUP = 35;
+var VUT_TOPO = 36;
+var VUT_ACTION = 37;
+var VUT_COUNT = 38;             /* Keep this last. */
 
 /* Sometimes we don't know (or don't care) if some requirements for effect
  * are currently fulfilled or not. This enum tells lower level functions

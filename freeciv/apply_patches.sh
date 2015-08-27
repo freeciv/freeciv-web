7aba6ca7becda27887a94b21ea46d5849b03a99c
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-08-27 02:09:42 +0200

    server: move glib config to network-rewrite-1
    All that remains of city_name_bugfix.patch is build system glib handling.
    Parts of network-rewrite-1.patch requires glib. Move the build system
    changes to network-rewrite-1.patch and delete city_name_bugfix.patch.

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -6,7 +6,7 @@
 # estring is Freeciv patch #6295.
 # estring-city-name is Freeciv patch #6296.
 
-PATCHLIST="estring estring-city-name freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city_name_bugfix city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix city_occupied_fix"
+PATCHLIST="estring estring-city-name freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix city_occupied_fix"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

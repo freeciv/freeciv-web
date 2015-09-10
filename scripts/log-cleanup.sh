af28997e3f781d7ad6cdca64661238133a0281b1
Andreas Røsdal <andreas.rosdal@gmail.com>
2015-09-10 19:19:10 +0200

    Clean up access logs.

--- scripts/log-cleanup.sh
+++ scripts/log-cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # cleans up logs
 
+rm /home/freeciv/freeciv-web/freeciv-web/resin/log/access.log*
 cp /dev/null /home/freeciv/freeciv-web/freeciv-web/resin/log/jvm-app-0.log
 cp /dev/null /home/freeciv/freeciv-web/freeciv-web/resin/log/access.log
 find /home/freeciv/freeciv-web/freeciv-web/logs/*.log -exec cp /dev/null {} \;

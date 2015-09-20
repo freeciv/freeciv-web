7874eb04b18c921d6f95c1ffc9fec7c32af5ee39
Marko Lindqvist <cazfi74@gmail.com>
2015-09-20 12:15:58 +0300

    Make AI units usable again after detecting inconsistent state in them
    Workaround to some crashes provided by freeciv_segfaults_fix.patch
    has been just bypassing problematic units forever. Fix their state so that
    AI can stop bypassing them.
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -2,7 +2,7 @@
 
 # activity_null_check is for Freeciv bug #22700.
 # city_traderoute_hotfix is for missing city save game crashes. Some were fixed in Freeciv SVN r29600. Others remain. (See Freeciv bug #23614)
-# freeciv_segfauls_fix fixes some segfaults in the Freeciv server. Freeciv bug #23884.
+# freeciv_segfauls_fix is a workaround some segfaults in the Freeciv server. Freeciv bug #23884.
 # GetTokensExactMax is for Freeciv bug #23887.
 
 PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix message_escape freeciv_segfauls_fix GetTokensExactMax"

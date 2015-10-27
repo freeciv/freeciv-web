2dcada46719fc95518a17c595ba647c8fe4d1215
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-10-28 00:50:45 +0100

    Disable the delta protocol via a configure option
    Disable the delta network protocol via the --disable-delta-protocol option.
    Remove the patch fragment that disabled it by patching generate_packets.py

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -10,8 +10,10 @@
 # do_not_offer_to_nuke_own_transport is Freeciv patch #6392.
 # spacerace.patch will be get partly obsoleted by Freeciv bug #22934
 # PlainFileBufSize1024.patch is Freeciv bug #23966
+# ignore-argument-position is Freeciv patch 6500
+# disable-delta-protocol is Freeciv patch 6501
 
-PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix message_escape freeciv_segfauls_fix GetTokensExactMax MapPosToTileOptimize scorelog_filenames serverside_actor_arrival do_not_offer_to_nuke_own_transport scorelog_set_to_client PlainFileBufSize1024"
+PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 webclient-ai-attitude current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace city_disbandable ai_traits_crash unittools ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets NoDeltaHeader city_traderoute_hotfix message_escape freeciv_segfauls_fix GetTokensExactMax MapPosToTileOptimize scorelog_filenames serverside_actor_arrival do_not_offer_to_nuke_own_transport scorelog_set_to_client PlainFileBufSize1024 ignore-argument-position disable-delta-protocol"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"
--- freeciv/prepare_freeciv.sh
+++ freeciv/prepare_freeciv.sh
@@ -29,5 +29,5 @@ fi
 
 ( cd freeciv
 
-  ./autogen.sh --with-project-definition=../freeciv-web.project --enable-fcweb --enable-json --disable-nls --disable-debug --disable-fcmp --disable-freeciv-manual --disable-ruledit --enable-ai-static=classic,threaded && make
+  ./autogen.sh --with-project-definition=../freeciv-web.project --enable-fcweb --enable-json --disable-delta-protocol --disable-nls --disable-debug --disable-fcmp --disable-freeciv-manual --disable-ruledit --enable-ai-static=classic,threaded && make
 )

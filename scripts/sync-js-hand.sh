ada42e43710329a416955bcdf779e179df774b2c
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2015-11-01 01:32:11 +0100

    Get the Europe 1901 scenario from Freeciv
    rather than carrying a local copy.

--- scripts/sync-js-hand.sh
+++ scripts/sync-js-hand.sh
@@ -4,7 +4,6 @@ python generate_js_hand/generate_js_hand.py && \
 cp packhand_gen.js ../freeciv-web/src/main/webapp/javascript/ && \
 cp packets.js ../freeciv-web/src/main/webapp/javascript/ && \
 cp ../freeciv/freeciv/data/scenarios/*.sav ../freeciv-web/src/main/webapp/savegames/ && \
-cp ../freeciv/data/europe_1901.sav ../freeciv-web/src/main/webapp/savegames/ && \
 python3.4 helpdata_gen/helpdata_gen.py &&\
 cp freeciv-helpdata.js ../freeciv-web/src/main/webapp/javascript/ && \
 cp ../LICENSE.txt ../freeciv-web/src/main/webapp/docs/ &&

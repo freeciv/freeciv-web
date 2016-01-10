99f8ca29395d1055e41155bf901088272179c36b
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-01-10 20:40:53 +0100

    Update to Freeciv SVN revision r30207

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # activity_null_check is for Freeciv bug #22700.
-# city_traderoute_hotfix is for missing city save game crashes. Some were fixed in Freeciv SVN r29600. Others remain. (See Freeciv bug #23614)
 # freeciv_segfauls_fix is a workaround some segfaults in the Freeciv server. Freeciv bug #23884.
 # message_escape is a patch for protecting against script injection in the message texts.
 # spacerace.patch will be get partly obsoleted by Freeciv bug #22934
@@ -15,8 +14,9 @@
 # json_memory is Freeciv patch #6634
 # may_die_during_fail is Freeciv bug #24026
 # disband_enabler is Freeciv patch #6573
+# missing_trade_partner is Freeciv patch #6814
 
-PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace ai_traits_crash ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets city_traderoute_hotfix message_escape freeciv_segfauls_fix scorelog_filenames scorelog_set_to_client PlainFileBufSize1024 ignore-argument-position disable-delta-protocol WWI_closest WWI_scibox WWI_ai WWI_description json_memory may_die_during_fail disband_enabler"
+PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace ai_traits_crash ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets message_escape freeciv_segfauls_fix scorelog_filenames scorelog_set_to_client PlainFileBufSize1024 ignore-argument-position disable-delta-protocol WWI_closest WWI_scibox WWI_ai WWI_description json_memory may_die_during_fail disband_enabler missing_trade_partner"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

b6228ec297ec1ca4d0222a570bfba7245bbf7b56
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-01-11 01:51:17 +0100

    Update to Freeciv SVN revision r30300

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -5,15 +5,13 @@
 # message_escape is a patch for protecting against script injection in the message texts.
 # spacerace.patch will be get partly obsoleted by Freeciv bug #22934
 # PlainFileBufSize1024.patch is Freeciv bug #23966
-# ignore-argument-position is Freeciv patch 6500
-# disable-delta-protocol is Freeciv patch 6501
 # WWI_description is Freeciv patch #6521
 # json_memory is Freeciv patch #6634
 # may_die_during_fail is Freeciv bug #24026
 # disband_enabler is Freeciv patch #6573
 # missing_trade_partner is Freeciv patch #6814
 
-PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace ai_traits_crash ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets message_escape freeciv_segfauls_fix scorelog_filenames scorelog_set_to_client PlainFileBufSize1024 ignore-argument-position disable-delta-protocol WWI_description json_memory may_die_during_fail disband_enabler missing_trade_partner"
+PATCHLIST="freeciv_web_all_packets_def_changes caravan_fixes1 city_fixes city_impr_fix2 city-naming-change city_fixes2 citytools_changes map-settings metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes network-rewrite-1 fcnet_packets misc_devversion_sync scenario_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks spacerace ai_traits_crash ruleset-capability worklists server_password aifill barbarian-names activity_null_check add_rulesets message_escape freeciv_segfauls_fix scorelog_filenames scorelog_set_to_client PlainFileBufSize1024 WWI_description json_memory may_die_during_fail disband_enabler missing_trade_partner"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

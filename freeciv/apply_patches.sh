12d22c52267c09a4199b7b76656258e0d1f994c0
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-06-24 02:45:42 +0200

    Update Freeciv server to r31962

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -4,9 +4,6 @@
 # freeciv_segfauls_fix is a workaround some segfaults in the Freeciv server. Freeciv bug #23884.
 # message_escape is a patch for protecting against script injection in the message texts.
 # tutorial_ruleset changes the ruleset of the tutorial to one supported by Freeciv-web.
-# Remove-redundant-canceled-orders-notification is Freeciv bug #24414 (SVN r31956)
-# metamessage_setting is Freeciv bug #24415 (SVN r31961)
-# settings_access_level_metamessage is replaced by Freeciv patch #6967 (SVN r31962)
 # AllowIextraInBetween is Freeciv bug #24395 (SVN r31970)
 # GenericPacketsGen24421 is json protocol change, backported from Freeciv bug #24421 (SVN r31996)
 # WarningsBase fixes compiler warnings from the code that gets properly replaced by GenericPacketsGen24421
@@ -29,10 +26,11 @@
 # BuildingCrucial is backport of Freeciv bug #24526 (SVN r32955)
 # win_chance includes 'Chance to win' in Freeciv-web map tile popup.
 # disable_global_warming is Freeciv bug #24418
+# fcweb_scorelog is Freeciv patch #7313
 # navajo-remove-long-city-names is a quick-fix to remove city names which would be longer than MAX_LEN_NAME when the name is url encoded in json protocol. A possibly better solution is to increase MAX_LEN_NAME.
 # MetaconnectionPersistent is Freeciv patch #7300 implementing persistent metaserver connections.
 
-PATCHLIST="WarningsBase freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks ai_traits_crash worklists server_password barbarian-names activity_null_check add_rulesets message_escape freeciv_segfauls_fix scorelog_filenames scorelog_set_to_client settings_access_level_metamessage metamessage_setting disable_global_warming GenericPacketsGen24421 ArrayDimensions CommentLineOfItsOwn Remove-redundant-canceled-orders-notification win_chance ScorefileWeb AllowIextraInBetween CardinalityCheckOnDemand ClassBonusRoadsCache InlineGenlistNavigation TileExtrasSafe NoNonnull SaveInvalidResource FairResourceExtra1 FairResourceExtra2 BuildingCrucial navajo-remove-long-city-names MetaconnectionPersistent"
+PATCHLIST="WarningsBase freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch serverside_extra_assign libtoolize_no_symlinks ai_traits_crash worklists server_password barbarian-names activity_null_check add_rulesets message_escape freeciv_segfauls_fix scorelog_filenames fcweb_scorelog disable_global_warming GenericPacketsGen24421 ArrayDimensions CommentLineOfItsOwn win_chance ScorefileWeb AllowIextraInBetween CardinalityCheckOnDemand ClassBonusRoadsCache InlineGenlistNavigation TileExtrasSafe NoNonnull SaveInvalidResource FairResourceExtra1 FairResourceExtra2 BuildingCrucial navajo-remove-long-city-names MetaconnectionPersistent"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

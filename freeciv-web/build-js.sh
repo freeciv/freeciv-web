741971ed9a0b1613e1d7c2796813d8f21bb0e459
Andreas Røsdal <andreas.rosdal@gmail.com>
2016-06-28 21:49:17 +0200

    Copy js source maps in build-js.sh script also.

--- freeciv-web/build-js.sh
+++ freeciv-web/build-js.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # builds javascript files Freeciv-web and copies the resulting file to tomcat.
 
-mvn compile && cp target/freeciv-web/javascript/webclient.min.js /var/lib/tomcat8/webapps/ROOT/javascript/
+mvn compile && cp target/freeciv-web/javascript/webclient.min.* /var/lib/tomcat8/webapps/ROOT/javascript/

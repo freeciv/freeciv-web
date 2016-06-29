ae075df8a71d1eb13a5ff965b407e725922918a3
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-06-29 08:56:16 +0200

    Update Freeciv server to r32268

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -4,9 +4,6 @@
 # freeciv_segfauls_fix is a workaround some segfaults in the Freeciv server. Freeciv bug #23884.
 # message_escape is a patch for protecting against script injection in the message texts.
 # tutorial_ruleset changes the ruleset of the tutorial to one supported by Freeciv-web.
-# FairResourceExtra1 is Freeciv bug #24533 (SVN r32262)
-# SaveInvalidResource is Freeciv bug #24534 (SVN r32263)
-# FairResourceExtra2 is Freeciv bug #24537 (SVN r32268)
 # CommentLineOfItsOwn is backport of Freeciv patch #7000 (SVN r32307)
 # CardinalityCheckOnDemand is backport of Freeciv patch #7126 (SVN r32462)
 # TileExtrasSafe is backport of Freeciv patch #7127 (SVN r32475)
@@ -26,7 +23,7 @@
 # fcweb_scorelog is Freeciv patch #7313 (SVN r33062)
 # disable_global_warming is Freeciv bug #24418
 
-PATCHLIST="freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch serverside_extra_assign ai_traits_crash worklists server_password barbarian-names activity_null_check add_rulesets message_escape freeciv_segfauls_fix scorelog_filenames fcweb_scorelog disable_global_warming CommentLineOfItsOwn win_chance ScorefileWeb CardinalityCheckOnDemand ClassBonusRoadsCache TileExtrasSafe NoNonnull SaveInvalidResource FairResourceExtra1 FairResourceExtra2 BuildingCrucial navajo-remove-long-city-names MetaconnectionPersistent"
+PATCHLIST="freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch serverside_extra_assign ai_traits_crash worklists server_password barbarian-names activity_null_check add_rulesets message_escape freeciv_segfauls_fix scorelog_filenames fcweb_scorelog disable_global_warming CommentLineOfItsOwn win_chance ScorefileWeb CardinalityCheckOnDemand ClassBonusRoadsCache TileExtrasSafe NoNonnull BuildingCrucial navajo-remove-long-city-names MetaconnectionPersistent"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

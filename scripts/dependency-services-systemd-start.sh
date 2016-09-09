68d83ae5eea4acfec6f5041ec9881cee9474454f
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-09-09 18:43:02 +0200

    scripts: start MySQL if it isn't running...
    ...when dependency-services-systemd-start.sh is used.

--- scripts/dependency-services-systemd-start.sh
+++ scripts/dependency-services-systemd-start.sh
@@ -3,6 +3,10 @@
 # Use systemd to start the services Freeciv-web depends on. Put this script
 # in configuration.sh's DEPENDENCY_SERVICES_START variable to use it.
 
+# Just in case it didn't start at boot time
+systemctl is-active --quiet mysql.service || systemctl start mysql.service
+
+# Is stopped and (re)started with Freeciv-web
 systemctl is-active --quiet nginx.service || systemctl start nginx.service
 systemctl is-active --quiet php7.0-fpm.service || systemctl start php7.0-fpm.service
 systemctl is-active --quiet tomcat8.service || systemctl start tomcat8.service

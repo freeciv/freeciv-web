d92a86b83c8e9ccd7f5d43455c72528674f38109
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-09-09 15:01:07 +0200

    Update to Freeciv server r33664.

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -2,7 +2,6 @@
 
 # Freeciv server version upgrade notes
 # ------------------------------------
-# Revert-Sanity-check-action-distance-range is replaced by Freeciv bug #24987 (SVN r33664).
 
 # Not in the upstream Freeciv server
 # ----------------------------------
@@ -25,7 +24,7 @@
 # freeciv_web_ruleset allows Freeciv-web to load rulesets not marked as web-compatible (civ2civ3). See discussion in patch #7514.
 # webperimental_install make "make install" install webperimental.
 
-PATCHLIST="0001-Revert-Added-packet-type-web_city_info_addition-to-b Revert-Sanity-check-action-distance-range freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch ai_traits_crash server_password barbarian-names activity_null_check message_escape freeciv_segfauls_fix scorelog_filenames disable_global_warming win_chance navajo-remove-long-city-names freeciv_web_ruleset webperimental_install "
+PATCHLIST="0001-Revert-Added-packet-type-web_city_info_addition-to-b freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch ai_traits_crash server_password barbarian-names activity_null_check message_escape freeciv_segfauls_fix scorelog_filenames disable_global_warming win_chance navajo-remove-long-city-names freeciv_web_ruleset webperimental_install "
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

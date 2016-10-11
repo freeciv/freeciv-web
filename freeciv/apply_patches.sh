cf0447886965c0c00515941ca4606220b760d247
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2016-10-11 20:42:38 +0200

    Explanation: "Conquer City" is city targeted.
    Check the target city's tile rather than a potentially missing target tile
    when trying to explain why the "Conquer City" action was illegal.
    
    Reported by Andreas Røsdal <andreasr>
    
    See Freeciv Gna bug #25170

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -23,8 +23,9 @@
 #     Giving one of the longer removed city names to a new city still causes problems.
 # freeciv_web_ruleset allows Freeciv-web to load rulesets not marked as web-compatible (civ2civ3). See discussion in patch #7514.
 # webperimental_install make "make install" install webperimental.
+# conquer_city_expl_fix is Freeciv Gna bug #25170
 
-PATCHLIST="0001-Revert-Added-packet-type-web_city_info_addition-to-b freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch ai_traits_crash server_password barbarian-names activity_null_check message_escape freeciv_segfauls_fix scorelog_filenames disable_global_warming win_chance navajo-remove-long-city-names freeciv_web_ruleset webperimental_install "
+PATCHLIST="0001-Revert-Added-packet-type-web_city_info_addition-to-b freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch ai_traits_crash server_password barbarian-names activity_null_check message_escape freeciv_segfauls_fix scorelog_filenames disable_global_warming win_chance navajo-remove-long-city-names freeciv_web_ruleset webperimental_install conquer_city_expl_fix"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

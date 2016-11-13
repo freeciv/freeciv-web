09f10ee59c850be49c6eafc6d3860eb70dafbbb2
Louis Moureaux <m_louis30@yahoo.com>
2016-11-13 02:37:59 +0100

    Add trees, mainly to forests and jungle tiles

--- freeciv-web/src/main/webapp/javascript/webgl/mapview.js
+++ freeciv-web/src/main/webapp/javascript/webgl/mapview.js
@@ -212,6 +212,53 @@ function render_testmap() {
 
   if (webgl_models["settler"] != null) scene.add(webgl_models["settler"]);
 
+  /* Trees */
+  var trees_geometry = new THREE.Geometry();
+  for (var i = 0, l = landGeometry.vertices.length; i < l; i++) {
+    var x = i % xquality, y = Math.floor(i / xquality);
+    var gx = Math.round(x / 4 - 0.5);
+    var gy = Math.round(y / 4 - 0.5);
+    var ptile = map_pos_to_tile(gx, gy);
+    if (ptile != null && tile_get_known(ptile) != TILE_UNKNOWN) {
+      var terrain_name = tile_terrain(ptile).name;
+      var add_tree = false;
+      if (terrain_name == "Forest" || terrain_name == "Jungle") {
+        /* Dense forests. */
+        add_tree = true;
+      } else if (terrain_name == "Grassland" ||
+                 terrain_name == "Plains" ||
+                 terrain_name == "Hills") {
+        /* Sparse trees -> Monte-Carlo algorithm */
+        add_tree = (Math.random() < 0.03);
+      }
+
+      /* No trees on beaches */
+      add_tree &= (landGeometry.vertices[i].y > 57);
+
+      if (add_tree) {
+        var geometry = new THREE.SphereGeometry(6, 4, 4);
+        geometry.rotateX(0.1 * Math.PI * Math.random());
+        geometry.rotateY(0.1 * Math.PI * Math.random());
+        geometry.rotateZ(0.1 * Math.PI * Math.random());
+        geometry.translate(landGeometry.vertices[i].x + 5,
+                           landGeometry.vertices[i].y + 2,
+                           landGeometry.vertices[i].z + 5);
+        geometry.translate(2.5 * (-1 + 2*Math.random()),
+                           0.1 * (-1 + 2*Math.random()),
+                           2.5 * (-1 + 2*Math.random()));
+
+        var mesh = new THREE.Mesh(geometry);
+        mesh.updateMatrix();
+        trees_geometry.merge(mesh.geometry, mesh.matrix);
+      }
+    }
+  }
+  var trees_material = new THREE.MeshLambertMaterial({
+      color: new THREE.Color(0, 0.15, 0),
+      emissive: new THREE.Color(0, 0.01, 0) });
+  var trees_mesh = new THREE.Mesh(trees_geometry, trees_material);
+  scene.add(trees_mesh);
+
   console.log("WebGL render_testmap took: " + (new Date().getTime() - start_webgl) + " ms.");
 }
 

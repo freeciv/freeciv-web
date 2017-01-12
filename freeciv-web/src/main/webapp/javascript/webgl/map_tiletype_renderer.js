e4f5bb4732b927dd479247de6dc9acfd1bbcb1e1
Louis Moureaux <m_louis30@yahoo.com>
2017-01-12 05:52:54 +0100

    Speed up generate_map_tiletype_grid by 20% on Chromium.
    It's even faster on Firefox.

--- freeciv-web/src/main/webapp/javascript/webgl/map_tiletype_renderer.js
+++ freeciv-web/src/main/webapp/javascript/webgl/map_tiletype_renderer.js
@@ -35,8 +35,8 @@ function generate_map_tiletype_grid() {
 
   for (var x = 0; x < map_tiletype_resolution ; x++) {
     for (var y = 0; y < map_tiletype_resolution; y++) {
-      var gx = Math.floor((map['ysize'] / map['xsize']) * x * map['xsize'] / map_tiletype_resolution);
-      var gy = Math.floor((map['xsize'] / map['ysize']) * y * map['ysize'] / map_tiletype_resolution);
+      var gx = Math.floor(map.ysize * x / map_tiletype_resolution);
+      var gy = Math.floor(map.xsize * y / map_tiletype_resolution);
       grid[x][y] = map_tiletype_tile_color(gy, gx);
     }
   }
@@ -47,8 +47,14 @@ function generate_map_tiletype_grid() {
   for (var i = 0; i < num_iterations; i++) {
     for (var x = 0; x < map_tiletype_resolution - 2; x++) {
       for (var y = 0; y < map_tiletype_resolution - 2; y++) {
-        if (Math.random() >= 0.6) {
-          grid[x][y] = grid[x + 2][y + 2];
+        var fx = map.ysize * x / map_tiletype_resolution;
+        var fy = map.xsize * y / map_tiletype_resolution;
+        fx -= Math.floor(fx); // Equivalent to % 1, but much faster
+        fy -= Math.floor(fy);
+        if (fx > 0.9 || fx < 0.1 && fy > 0.9 || fy < 0.1) {
+          if (Math.random() >= 0.6) {
+            grid[x][y] = grid[x + 2][y + 2];
+          }
         }
       }
     }

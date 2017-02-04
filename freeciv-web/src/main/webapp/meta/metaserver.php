40ae7d2d046020887892bd6781c7bab757e2371e
Andreas Røsdal <andreas.rosdal@gmail.com>
2017-02-04 20:03:46 +0100

    Add code from original metaserver.

--- freeciv-web/src/main/webapp/meta/metaserver.php
+++ freeciv-web/src/main/webapp/meta/metaserver.php
@@ -100,6 +100,19 @@ if ( isset($port) ) {
     exit(1);
   }
 
+  /* This is to check if the name they have supplied matches their IP */
+    /* Maybe they have a proxy they can't get around */
+  if ( isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ) {
+    $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+  } elseif ( isset($_SERVER["HTTP_CLIENT_IP"]) ) {
+    $ip = $_SERVER["HTTP_CLIENT_IP"];
+  } else {
+    $ip = $_SERVER["REMOTE_ADDR"];
+  }
+
+  if (gethostbyname($host) != $ip) {
+    $host = @gethostbyaddr($ip);
+  }
 
   /* is this server going away? */
   if (isset($bye)) {

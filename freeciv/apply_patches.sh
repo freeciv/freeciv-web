937c788b01f2d26e92202197f0b8beec4cbb15b9
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2017-04-23 15:18:04 +0200

    Update to Freeciv server r35275.
    Freeciv commit message:
    Fix build when --enable-sys-tolua-cmd=test does not find tolua
    
    Reported by Sveinung Kvilhaugsvik
    
    See hrm bug #653989

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -26,9 +26,8 @@
 # webgl_vision_cheat_temporary is a temporary solution to reveal terrain types to the WebGL client.
 # user_changeable_gameseed allows user to change map and game seed, to get reliable benchmarks of WebGL version.
 # longturn implements a very basic longturn mode for Freeciv-web. Longturn port number is (6003).
-# ToluaTestToFalse (hrm Bug #653989) fixes building without system tolua.
 
-PATCHLIST="0001-Revert-Added-packet-type-web_city_info_addition-to-b freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch ai_traits_crash server_password barbarian-names activity_null_check message_escape freeciv_segfauls_fix scorelog_filenames disable_global_warming win_chance navajo-remove-long-city-names freeciv_web_ruleset webperimental_install webgl_vision_cheat_temporary user_changeable_gameseed longturn ToluaTestToFalse"
+PATCHLIST="0001-Revert-Added-packet-type-web_city_info_addition-to-b freeciv_web_packets_def_changes city_fixes city_impr_fix2 city-naming-change city_fixes2 metachange text_fixes unithand-change2 current_research_cost freeciv-svn-webclient-changes init_lists_disable goto_fcweb misc_devversion_sync tutorial_ruleset savegame maphand_ch ai_traits_crash server_password barbarian-names activity_null_check message_escape freeciv_segfauls_fix scorelog_filenames disable_global_warming win_chance navajo-remove-long-city-names freeciv_web_ruleset webperimental_install webgl_vision_cheat_temporary user_changeable_gameseed longturn"
 
 apply_patch() {
   echo "*** Applying $1.patch ***"

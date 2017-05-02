ba4228b38c09b1d9c5f905056d2520d5376a7a55
Andreas Røsdal <andreas.rosdal@gmail.com>
2017-05-02 19:33:16 +0200

    Copy also non-minified JavaScript files.

--- freeciv-web/build-js.sh
+++ freeciv-web/build-js.sh
@@ -5,7 +5,7 @@
 rm -rf target/freeciv-web/javascript/webclient.min.js.map
 rm -rf target/freeciv-web/javascript/webgl/libs/webgl-client.min.js.map
 
-mvn compile && cp target/freeciv-web/javascript/webclient.min.* /var/lib/tomcat8/webapps/ROOT/javascript/ && cp target/freeciv-web/javascript/webgl/libs/webgl-client.min* /var/lib/tomcat8/webapps/ROOT/javascript/webgl/libs/
+mvn compile && cp target/freeciv-web/javascript/webclient.* /var/lib/tomcat8/webapps/ROOT/javascript/ && cp target/freeciv-web/javascript/webgl/libs/webgl-client* /var/lib/tomcat8/webapps/ROOT/javascript/webgl/libs/
 
 # update timestamp to clear browser cache.
 sed -i.bak -e "s/ts=\"/ts=\"1/" -e "s/\?ts=/\?ts=1/" /var/lib/tomcat8/webapps/ROOT/webclient/index.jsp 

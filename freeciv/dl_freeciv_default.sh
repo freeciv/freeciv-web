cfc02f53f8c3bfb817c59fb85f3fef4f88096604
Andreas Røsdal <andreas.rosdal@gmail.com>
2017-05-26 22:30:51 +0200

    Use temporary git mirror for Freeciv server repo.

--- freeciv/dl_freeciv_default.sh
+++ freeciv/dl_freeciv_default.sh
@@ -4,4 +4,9 @@
 # This will happen unless the script dl_freeciv.sh exists.
 # If you wish to modify this file copy it to dl_freeciv.sh and edit it.
 
-svn --quiet export svn://svn.gna.org/svn/freeciv/$2 -r $1 freeciv
+#svn --quiet export svn://svn.gna.org/svn/freeciv/$2 -r $1 freeciv
+
+#Temporary git repo for Freeciv. 
+#Note to self: say thanks to JTN for setting up new Git repo for Freeciv.
+git clone git://repo.or.cz/freeciv.git  --depth=10
+

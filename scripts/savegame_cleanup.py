22f2d71fd2da03c3cf238263cb4c8304096eef8f
Andreas Røsdal <andreas.rosdal@gmail.com>
2017-06-01 16:59:44 +0200

    Clean up autosaves better.

--- scripts/savegame_cleanup.py
+++ scripts/savegame_cleanup.py
@@ -19,6 +19,7 @@ scenario_list = ['british-isles-85x80-v2.80.sav.gz', 'iberian-peninsula-136x100-
 
 keeptime = 60*60*24*365;
 keeptime_pbem = 60*60*24*10;
+keeptime_autosave = 60*60*24*5;
 
 for root, subFolders, files in os.walk(rootdir):
     for file in files:
@@ -32,4 +33,9 @@ for root, subFolders, files in os.walk(rootdir):
         if difftime > keeptime_pbem and (file.endswith(".xz") or file.endswith(".sav")) and file.startswith("pbem") and file not in scenario_list:
           print("rm file: " + file);
           os.unlink(f)
-
+        if difftime > keeptime_autosave and file.endswith("auto.sav.xz"):
+          print("rm file: " + file);
+          os.unlink(f)
+        if difftime > keeptime_autosave and file.startswith("freeciv-earth-savegame"):
+          print("rm file: " + file);
+          os.unlink(f)          

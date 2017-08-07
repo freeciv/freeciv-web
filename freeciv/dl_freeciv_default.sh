5343cead69af14e0417abab8c785c5044b3e3a10
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2017-08-07 12:13:04 +0200

    Don't redownload Freeciv on each rebuild.
    Store Freeciv's master branch in the git branch freeciv_master after
    downloading it. Freeciv-web uses Freeciv master. It can therefore be assumed
    that Freeciv-web's current Freeciv server revision is an ancestor of Freeciv
    master.
    
    This solution doesn't put the Freeciv server under Freeciv-web's git control
    like git subtree would have done.
    
    Freeciv-web patches the Freeciv source code after downloading it. Having the
    modifications as patches rather than commits (with new commits on top) makes
    it easier to merge Freeciv-web specific changes.
    
    If Freeciv-web's git keeps track of Freeciv's files the applied patches could
    end up being committed. (It can't be assumed that all developers would avoid
    adding the automatically patched Freeciv files to the index before
    committing.)
    
    That problem could of course have been avoided by manually copying the
    freeciv subtree before applying the patches but that would have been
    pointless.
    
    This solution doesn't require the user to run any special commands after the
    Freeciv server version has been updated to avoid reverting the update in the
    next commit.
    
    The part of the solution that manually downloads Freeciv's master could have
    been replaced by setting the wanted Freeciv commit as a parent of the
    Freeciv-web update commit (without changing any files) each time the Freeciv
    revision was updated. This would be slightly more decentralized. On the
    other hand it would make updating the Freeciv server more complicated. The
    added complexity would be more work and leave more room for error. It is
    therefore, IMHO, not worth it.
    
    See hrm Task #656700

--- freeciv/dl_freeciv_default.sh
+++ freeciv/dl_freeciv_default.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-# Downloads the wanted revision of Freeciv via Git.
-# This will happen unless the script dl_freeciv.sh exists.
-# If you wish to modify this file copy it to dl_freeciv.sh and edit it.
+# Places the specified revision of Freeciv in freeciv/freeciv/
+# This is the default. The script dl_freeciv.sh will run instead of
+# dl_freeciv_default.sh if it exists.
+# If you want to modify this file copy it to dl_freeciv.sh and edit it.
 
-mkdir -p freeciv
-cd freeciv
-git init
-git fetch https://github.com/freeciv/freeciv.git :$1
-git reset --hard  $1
+# Download the wanted Freeciv revision from GitHub unless it is here already.
+# The download step saves having to merge in Freeciv's history each time the
+# Freeciv server revision is updated.
+git cat-file -e $1 || git fetch --no-tags https://github.com/freeciv/freeciv.git master:freeciv_master
 
+# Place the requested Freeciv revision in the freeciv/freeciv folder.
+# The checkout isn't owned by git. This means that the patches automatically
+# applied during the build won't accidentally end up in commits. It also
+# means that committing unrelated changes won't accidentally revert the
+# Freeciv server revision because a command didn't run.
+git read-tree --prefix=freeciv/freeciv/ --index-output=.freeciv_index $1
+GIT_INDEX_FILE=.freeciv_index git checkout-index -af
+rm -f .freeciv_index

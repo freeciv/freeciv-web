275ad16e37293e8e3b1f00c64f0c0f4e65379d68
Máximo Castañeda <antiswen@yahoo.es>
2017-08-10 21:00:43 +0200

    Remove vbox-workaround-16670.sh
    The script was a workaround for a problem in VirtualBox 5.1.20, a version that nobody seems to be shipping anymore. The script itself has been disabled for more than two months, so deleting.

--- scripts/vbox-workaround-16670.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Detect and correct bug in vbox guest additions 5.1.20
-# See https://www.virtualbox.org/ticket/16670
-
-[ -e "/sbin/mount.vboxsf" ] || sudo ln -sf /opt/VBoxGuestAdditions-5.1.20/lib/VBoxGuestAdditions/mount.vboxsf /sbin/mount.vboxsf
-mount -t vboxsf -o uid=`id -u ubuntu`,gid=`id -g ubuntu` vagrant /vagrant

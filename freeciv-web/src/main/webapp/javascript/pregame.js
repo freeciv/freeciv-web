501b291a375114d236bfb7675570e2d3134766ea
Andreas Røsdal <andreas.rosdal@gmail.com>
2017-09-10 20:28:55 +0200

    Remove unused code.

--- freeciv-web/src/main/webapp/javascript/pregame.js
+++ freeciv-web/src/main/webapp/javascript/pregame.js
@@ -1218,13 +1218,6 @@ function show_longturn_intro_dialog() {
 
 		});
 
-  var stored_username = simpleStorage.get("username", "");
-  var stored_password = simpleStorage.get("password", "");
-  if (stored_username != null && stored_username != false && stored_password != null && stored_password != false) {
-    // Not allowed to create a new user account when already logged in.
-    $(".ui-dialog-buttonset button").last().button("disable");
-  }
-
   if (is_small_screen()) {
     /* some fixes for pregame screen on small devices.*/
     $("#freeciv_logo").remove();

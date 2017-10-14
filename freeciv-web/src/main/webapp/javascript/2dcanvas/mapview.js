71453877f24d64c650789a64566ca7b8f3be261d
Máximo Castañeda <antiswen@yahoo.es>
2017-10-14 21:55:03 +0200

    Fix typo in imageSmoothingEnabled check

--- freeciv-web/src/main/webapp/javascript/2dcanvas/mapview.js
+++ freeciv-web/src/main/webapp/javascript/2dcanvas/mapview.js
@@ -69,7 +69,7 @@ function init_mapview()
   buffer_canvas = document.createElement('canvas');
   buffer_canvas_ctx = buffer_canvas.getContext('2d');
 
-  if ("imageSmoothingEnabled " in mapview_canvas_ctx) {
+  if ("imageSmoothingEnabled" in mapview_canvas_ctx) {
     // if this Boolean value is false, images won't be smoothed when scaled. This property is true by default.
     mapview_canvas_ctx.imageSmoothingEnabled = false;
   }

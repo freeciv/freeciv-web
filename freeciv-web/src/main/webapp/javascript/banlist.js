dbc8aa10f668f9006394d482bd9cd4d99e05daa8
Andreas Røsdal <andreas.rosdal@gmail.com>
2017-11-08 06:11:24 +0100

    Update

--- freeciv-web/src/main/webapp/javascript/banlist.js
+++ freeciv-web/src/main/webapp/javascript/banlist.js
@@ -21,7 +21,7 @@
 /* This is a list of banned users of Freeciv-web.
    Note that user accounts can also be disabled by setting activated=0 in the auth DB table.
 */
-var banned_users = [];
+var banned_users = ["Corbeau"];
 
 /**************************************************************************
  Returns false if the text contains a banned user.

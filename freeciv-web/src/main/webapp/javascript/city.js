11a77384643f51659c70476dc0161cf6e77c9e0e
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2019-12-05 04:32:08 +0100

    Display culture value.
    Rulesets like Civ2Civ3 enables culture. Show it to the player.

--- freeciv-web/src/main/webapp/javascript/city.js
+++ freeciv-web/src/main/webapp/javascript/city.js
@@ -362,6 +362,7 @@ function show_city_dialog(pcity)
     $("#city_waste").html(pcity['waste'][O_SHIELD]);
     $("#city_pollution").html(pcity['pollution']);
     $("#city_steal").html(pcity['steal']);
+    $("#city_culture").html(pcity['culture']);
   }
 
   /* Handle citizens and specialists */

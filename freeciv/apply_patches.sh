df5334e1d30f52fc9b3a8444579e33f084298cfd
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2019-12-08 21:02:57 +0100

    mapimg_bugfix is gone

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -26,7 +26,6 @@
 # load_command_confirmation adds a log message which confirms that loading is complete, so that Freeciv-web can issue additional commands.
 # pragma_pack_city_length adds pragma pack to city packet. Also sets MAX_LEN_CITYNAME 50 for large longturn games.
 # endgame-mapimg is used to generate a mapimg at endgame for hall of fame.
-# mapimg_bugfix is http://www.hostedredmine.com/issues/707912.
 
 declare -a PATCHLIST=(
   "freeciv_web_packets_def_changes"

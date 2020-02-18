075dfca3280065866483145abecc3a7d12ab4d2b
Máximo Castañeda <antiswen@yahoo.es>
2020-02-18 12:48:20 +0100

    Run all tests and fail if any of them fails

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -2,6 +2,17 @@
 
 # Freeciv-web Travis CI test script
 
+error=0
+
+casper_test () {
+  xvfb-run --server-args="-screen 0 800x600x24" casperjs --engine=phantomjs test "$@"
+  err=$?
+  if [ $err -ne 0 ]; then
+    >&2 echo "Freeciv-web CasperJS test $@ failed with exit code ${err}"
+    error=1
+  fi
+}
+
 basedir=$(pwd)
 
 cd ${basedir}/scripts/ && ./start-freeciv-web.sh
@@ -12,17 +23,25 @@ echo "============================================"
 echo "Start testing of Freeciv-web using CasperJS:"
 cd ${basedir}/tests/
 
-xvfb-run --server-args="-screen 0 800x600x24" casperjs --engine=phantomjs test freeciv-web-tests.js || { >&2 echo "Freeciv-web CasperJS tests failed with exit code ${?}!" && exit 1; }
+casper_test freeciv-web-tests.js
 
 echo "Running Freeciv-web server in autogame mode."
+mv "${basedir}"/publite2/pubscript_singleplayer.serv{,.bak}
 cp ${basedir}/publite2/pubscript_autogame.serv ${basedir}/publite2/pubscript_singleplayer.serv
 killall freeciv-web
 sleep 20
-xvfb-run --server-args="-screen 0 800x600x24" casperjs --engine=phantomjs test freeciv-web-autogame.js || { >&2 echo "Freeciv-web CasperJS autogame tests failed with exit code ${?}!" && exit 1; }
+casper_test freeciv-web-autogame.js
+mv "${basedir}"/publite2/pubscript_singleplayer.serv{.bak,}
 
 echo "running pbem unit tests."
 cd ${basedir}/pbem
-python3 test_pbem.py
+if ! python3 test_pbem.py; then
+  error=1
+fi
 
 #echo "=============================="
-echo "Freeciv-web built, tested and started correctly: Build successful!"
+if [ ${error} -eq 0 ]; then
+  echo "Freeciv-web built, tested and started correctly: Build successful!"
+else
+  exit 1
+fi

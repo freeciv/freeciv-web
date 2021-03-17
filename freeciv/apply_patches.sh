dd58970817bf2d3b812dd0bb1cf0fa02a37c36f4
Marko Lindqvist <cazfi74@gmail.com>
2021-03-17 06:21:29 +0200

    Clean out more server compiler warnings
    Backport a couple more compiler warning fixes.
    Apply these patches first, so they are in effect when working
    on later patches of partly applied stack.
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -32,6 +32,10 @@
 #     It was committed as 7a57cea0673432472946956d9f8c635e9f165da2
 # 0025-Do-end_phase-research-updates-for-alive-players-only.patch is hrm Bug #873692
 #     It was committed as 833d1fb14c3a5014bc4eeccfe652f5d9dddf3119
+# 0009-Fix-cvercmp-compiler-warning-with-gcc-10-and-O3.patch is hrm Bug #886330
+#     It was committed as 7527315835c16179e68080fe8f7244c76152b656
+# 0010-Fix-stdinhand.c-compiler-warning-with-gcc-10-and-O3.patch is hrm Bug #886331
+#     It was committed as 71eb308f799fd62920077d68d9109448d47cc7a8
 
 # Not in the upstream Freeciv server
 # ----------------------------------
@@ -55,6 +59,12 @@
 # endgame-mapimg is used to generate a mapimg at endgame for hall of fame.
 
 declare -a PATCHLIST=(
+  "0003-Switch-from-python-to-python3"
+  "0009-Fix-cvercmp-compiler-warning-with-gcc-10-and-O3"
+  "0001-Terminate-format-escapes-list"
+  "0024-mapimg_colortest-Fix-compiler-warning-with-O3"
+  "0001-Refactor-code-to-avoid-gcc-10-warning"
+  "0010-Fix-stdinhand.c-compiler-warning-with-gcc-10-and-O3"
   "city_impr_fix2"
   "city-naming-change"
   "metachange"
@@ -84,16 +94,12 @@ declare -a PATCHLIST=(
   "webgl_vision_cheat_temporary"
   "endgame-mapimg"
   "0001-Fix-segfault-at-loading-older-format-savegame"
-  "0003-Switch-from-python-to-python3"
   "0015-savegame3.c-Remove-aifill-players-after-rulesets-loa"
-  "0001-Terminate-format-escapes-list"
-  "0001-Refactor-code-to-avoid-gcc-10-warning"
   "0023-Fix-sell_random_unit-crash-with-recursive-transports"
   "0005-Fix-ghost-unit-issue-when-unit-is-loaded-to-an-trans"
   "0001-Fix-clang-9-warnings"
   "0001-Fix-division-by-zero-when-transforming-unit-with-zer"
   "0006-debug.m4-Set-always-active-compiler-flags-last-not-f"
-  "0024-mapimg_colortest-Fix-compiler-warning-with-O3"
   "0001-Set-player-tile-owner-whenever-tile-knowledge-is-upd"
   "0025-Do-end_phase-research-updates-for-alive-players-only"
 )

25fa382aa9f7d97a351e0b8d0fe5ebb96214b0a1
Andreas Røsdal <andreas.rosdal@gmail.com>
2021-04-05 10:40:52 +0100

    Minor update to WebGl water rendering.

--- freeciv-web/src/main/webapp/javascript/webgl/mapview.js
+++ freeciv-web/src/main/webapp/javascript/webgl/mapview.js
@@ -128,8 +128,8 @@ function init_webgl_mapview() {
       flowDirection: new THREE.Vector2( 0.1, -0.1),
       textureWidth: 1024,
       textureHeight: 1024,
-      reflectivity : 0.4,
-      clipBias : 0.8,
+      reflectivity : 0.08,
+      clipBias : 0.1,
       normalMap0 : textureLoader.load( '/textures/Water_1_M_Normal.jpg' ),
       normalMap1 : textureLoader.load( '/textures/Water_2_M_Normal.jpg' )
 

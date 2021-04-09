b248dce56728c5bb24383a6791efc39e9087dc46
Marko Lindqvist <cazfi74@gmail.com>
2021-04-09 16:04:29 +0300

    Update to Freeciv server freeciv/freeciv@967ccf77
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv-web/src/main/webapp/javascript/fc_types.js
+++ freeciv-web/src/main/webapp/javascript/fc_types.js
@@ -194,7 +194,8 @@ var VUT_MINTECHS = 39;
 var VUT_EXTRAFLAG = 40;
 var VUT_MINCALFRAG = 41;
 var VUT_SERVERSETTING = 42;
-var VUT_COUNT = 43;             /* Keep this last. */
+var VUT_CITYSTATUS = 43;
+var VUT_COUNT = 44;             /* Keep this last. */
 
 /* Freeciv's gui_type enum */
 /* Used for options which do not belong to any gui. */

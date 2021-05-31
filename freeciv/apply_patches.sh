83fec678abe15f8ce7c5283b8a1471d50d13d02d
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2021-05-31 09:08:34 +0200

    Update to Freeciv server freeciv/freeciv@67383deed6
    Freeciv commit message:
    Correct wrong moves_actor_kind.
    
    The actions "Investigate City" and "Establish Embassy" doesn't do diplomat
    escape. The actor unit stays in place.
    
    Set their moves_actor_kind to MAK_STAYS rather than MAK_ESCAPE. Don't charge
    all movement as the spy escape cost.
    
    See osdn #41811

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -2,8 +2,6 @@
 
 # Freeciv server version upgrade notes
 # ------------------------------------
-# 0001-Correct-wrong-moves_actor_kind.patch is osdn #41811
-#     It was committed as 67383deed6794494fb1937aca661578879c3203c
 # 0001-Make-generated-random-seed-less-predictable.patch is hrm Bug #914184
 #     It was committed as a56144bd28e2a19707cee5c5c3028a12c634d0ff
 
@@ -28,7 +26,6 @@
 # endgame-mapimg is used to generate a mapimg at endgame for hall of fame.
 
 declare -a PATCHLIST=(
-  "0001-Correct-wrong-moves_actor_kind"
   "0001-Make-generated-random-seed-less-predictable"
   "city_impr_fix2"
   "city-naming-change"

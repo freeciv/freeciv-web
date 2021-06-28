freeciv/freeciv-web

commit 169b29c694fb47adff1dde32c8d6de23de961f87

act_sel_queue_done(): more paranoid clean up.

freeciv-web/src/main/webapp/javascript/action_dialog.js
@@ -65,6 +65,8 @@ function act_sel_queue_done(actor_unit_id)
 {
   /* Stop waiting. Move on to the next queued unit. */
   is_more_user_input_needed = false;
+  action_selection_restart = false;
+  did_not_decide = false;
   act_sel_queue_may_be_done(actor_unit_id);
 }
 

commit 7e0fca2334cc6da35da8b36bb4e6f75eadafc68a

act_sel_queue_done(): fix ordering.
Don't clear action_selection_restart and did_not_decide until after
act_sel_queue_may_be_done() has run and potentially used them.

Co-Authored-By: Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
Fixes: 169b29c694fb47adff1dde32c8d6de23de961f87

freeciv-web/src/main/webapp/javascript/action_dialog.js
@@ -65,9 +65,9 @@ function act_sel_queue_done(actor_unit_id)
 {
   /* Stop waiting. Move on to the next queued unit. */
   is_more_user_input_needed = false;
+  act_sel_queue_may_be_done(actor_unit_id);
   action_selection_restart = false;
   did_not_decide = false;
-  act_sel_queue_may_be_done(actor_unit_id);
 }
 
 /**************************************************************************
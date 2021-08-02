a4b8d887470eb16934acd5e6dc26f7eab1fe5699
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2021-08-02 06:13:09 +0200

    Update to Freeciv server freeciv/freeciv@704b91df21
    Freeciv commit message:
    Need war on target tile owner explanation.
    
    Action not enabled explain when war on the tile owner is needed because of a
    DiplRelTileOther requirement in an action enabler's target requirements.
    
    See osdn #42551

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -6,6 +6,8 @@
 #     It was committed as a53773bf84bd86ce181ba6f231900026ec1f5d9b
 # 0001-JSON-avoid-integer-underflow-on-orders is osdn #42595
 #     It was committed as cedd5f7a5af8a10445f01b09bd91794e0126101d
+# 0001-Stop-need_war_player_hlp-from-crashing.patch is osdn #42597
+#     It was committed as b3ac633befa215514040aeecbc3e81125c025ccb
 
 # Not in the upstream Freeciv server
 # ----------------------------------
@@ -28,6 +30,7 @@
 # endgame-mapimg is used to generate a mapimg at endgame for hall of fame.
 
 declare -a PATCHLIST=(
+  "0001-Stop-need_war_player_hlp-from-crashing"
   "0001-Map-unit-move-orders-to-dis-embark-actions"
   "0001-JSON-avoid-integer-underflow-on-orders"
   "city_impr_fix2"

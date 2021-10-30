8c8d388c046e0cc094d4ac204a46bf21789b299c
Marko Lindqvist <cazfi74@gmail.com>
2021-10-30 21:45:07 +0300

    Support using mariadb service instead of mysql one
    Even prefer it, as it's where we're going.
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- scripts/install/dependency-services-default-start.sh
+++ scripts/install/dependency-services-default-start.sh
@@ -11,7 +11,7 @@ export JAVA_OPTS="-Djava.security.egd=file:/dev/urandom"
 export CATALINA_HOME=/var/lib/tomcat9
 
 # 0. mysql
-pidof mysqld > /dev/null || sudo service mysql start
+pidof mysqld > /dev/null || sudo service mariadb start || sudo service mysql start
 
 # 1. nginx
 echo "Starting nginx first."

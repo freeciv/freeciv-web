d435609c4d02e5a87e6dd5e04441e972772c3592
Marko Lindqvist <cazfi74@gmail.com>
2022-08-04 22:42:07 +0300

    Update to Freeciv server freeciv/freeciv@25fde27381
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -2,9 +2,6 @@
 
 # Freeciv server version upgrade notes
 # ------------------------------------
-# 0008-Prohibit-changing-ruleset-given-from-commandline
-#   Work toward removal of hardcoded restriction to never allow ruleset change in freeciv-web
-#   osdn #45042
 # 0056-establish_new_connection-Set-wrap_id-to-packet_set_t.patch
 #   Fix regression introduced with the new separate wrap setting
 #   osdn #45261
@@ -36,9 +33,7 @@
 # endgame-mapimg is used to generate a mapimg at endgame for hall of fame.
 
 declare -a PATCHLIST=(
-  "0008-Prohibit-changing-ruleset-given-from-commandline"
   "0023-Meson-Detect-MagickWand"
-  "0046-Fix-json-delta-protocol-combination"
   "0001-Fix-JSON-protocol-regressions-introduced-in-hrm-745593"
   "0056-establish_new_connection-Set-wrap_id-to-packet_set_t"
   "meson_webperimental"

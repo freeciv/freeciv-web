46d59902f7a69fdf42c3a8e1f18f483f3f90e446
Marko Lindqvist <cazfi74@gmail.com>
2023-10-05 00:00:14 +0300

    freeciv-img-extract: Log which .svg you are converting
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- scripts/freeciv-img-extract/sync.sh
+++ scripts/freeciv-img-extract/sync.sh
@@ -28,9 +28,11 @@ pngcrush -q -d "${TILESET_DEST}" "${TEMP_DIR}"/freeciv-web-tileset*.png &&
 cp "${TEMP_DIR}"/tileset_spec_*.js "${SPEC_DEST}" &&
 echo "converting flag .svg files to .png ..." &&
 (for svgfile in $(find "${FREECIV_DIR}"/data/flags/*.svg); do
-  name="$(basename "$svgfile")"
+  name="$(basename "${svgfile}")"
   pngfile="${FLAG_DEST}/${name/%.svg/-web.png}"
-  timeout -k 3 8 convert -density 80 -resize 180 "$svgfile" "${pngfile}" ||
+  echo "${svgfile} -> ${pngfile}"
+  timeout -k 3 8 convert -density 80 -resize 180 "${svgfile}" "${pngfile}" ||
     >&2 echo "  ERROR converting ${svgfile} to ${pngfile}"
 done) &&
 echo "Freeciv-img-extract done." || (>&2 echo "Freeciv-img-extract failed!" && exit 1)
+sleep 120

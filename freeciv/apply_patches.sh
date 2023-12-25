6fffbfd241bda75e0167e06965b1d9a6c77b9746
Marko Lindqvist <cazfi74@gmail.com>
2023-12-25 20:23:12 +0200

    Update to Freeciv server freeciv/freeciv@e8beaa91d5
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -2,22 +2,10 @@
 
 # Freeciv server version upgrade notes (backports)
 # ------------------------------------------------
-# osdn #????? or RM #? is a ticket in freeciv.org tracker:
+# osdn #????? or RM #?? is a ticket in freeciv.org tracker:
 # https://osdn.net/projects/freeciv/ticket/?????
 # https://redmine.freeciv.org/issues/??
 #
-# 0039-Improve-report.c-coding-style.patch
-#   Baseline for scorelog_filenames.patch
-#   osdn #48949
-# 0002-Pick-random-nations-before-setting-turn-number-to-1.patch
-#   Assert fix
-#   ?????
-# 0003-Reserve-space-for-terminating-NULL-on-astr_buffer.patch
-#   String handling / datafile loading fix
-#   ?????
-# 0005-Meson-Define-SIZEOF_INT.patch
-#   Fix to meson based build
-#   RM #7
 # 0029-Meson-Search-mwand-by-MagickWand-6.Q16HDRI.pc.patch
 #   MagickWand configure time fix on latest Debian/Ubuntu
 #   RM #32
@@ -52,10 +40,6 @@ declare -a GIT_PATCHLIST=(
 )
 
 declare -a PATCHLIST=(
-  "backports/0039-Improve-report.c-coding-style"
-  "backports/0002-Pick-random-nations-before-setting-turn-number-to-1"
-  "backports/0003-Reserve-space-for-terminating-NULL-on-astr_buffer"
-  "backports/0005-Meson-Define-SIZEOF_INT"
   "backports/0029-Meson-Search-mwand-by-MagickWand-6.Q16HDRI.pc"
   "backports/0027-worklist_item_postpone_req_vec-Add-break-for-VUT_PLA"
   "backports/0029-universal_value_initial-Fix-switch-case-fall-through"
